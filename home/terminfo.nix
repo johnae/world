7457ff989bcb37cf515afa4163f29bc40d70eba9
John Axel Eriksson <john@insane.se>
2020-07-12 10:02:06 +0200

    Fix terminfo

--- home/terminfo.nix
+++ home/terminfo.nix
@@ -1,8 +1,6 @@
 { pkgs, config, lib, options }:
-
-{
-
-  xdg.configFile."terminfo/xterm-24bits.conf".text = ''
+let
+  terminfoDefinition = pkgs.writeText "terminfoDefs" ''
     # Use colon separators.
     xterm-24bit|xterm with 24-bit direct color mode,
       use=xterm-256color,
@@ -14,11 +12,12 @@
       setb24=\E[48;2;%p1%{65536}%/%d;%p1%{256}%/%{255}%&%d;%p1%{255}%&%dm,
       setf24=\E[38;2;%p1%{65536}%/%d;%p1%{256}%/%{255}%&%d;%p1%{255}%&%dm,
   '';
-
-  xdg.configFile."terminfo/xterm-24bits.conf".onChange = ''
-    for file in ~/.config/terminfo/*; do
-      ${pkgs.ncurses}/bin/tic -x -o ~/.terminfo "$file"
-    done
+  terminfo = pkgs.runCommand "terminfo" { } ''
+    ${pkgs.ncurses}/bin/tic -x -o $out "${terminfoDefinition}"
   '';
+in
+{
+
+  home.file.".terminfo".source = terminfo;
 
 }
