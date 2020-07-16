64c04cfa50c55f817b08fc115ee202a2f142329b
John Axel Eriksson <john@insane.se>
2020-07-16 22:37:25 +0200

    Update initialize-user

--- pkgs/initialize-user/default.nix
+++ pkgs/initialize-user/default.nix
@@ -15,10 +15,16 @@ writeStrictShellScriptBin "initialize-user" ''
 
   chmod 0700 .gnupg
 
-  if [ ! -e Development/nixos-configuration ]; then
-    git clone --recursive git@github.com:johnae/nixos-configuration Development/nixos-configuration
+  if [ ! -e Development/world ]; then
+    git clone --recursive git@github.com:johnae/world Development/world
   else
-    echo nixos-configuration already exists at Development/nixos-configuration
+    echo world already exists at Development/world
+  fi
+
+  if [ ! -e Development/nixos-metadata ]; then
+    git clone --recursive git@github.com:johnae/nixos-metadata Development/nixos-metadata
+  else
+    echo world already exists at Development/nixos-metadata
   fi
 
   if [ ! -e "$PASSWORD_STORE_DIR" ]; then
@@ -32,9 +38,13 @@ writeStrictShellScriptBin "initialize-user" ''
 
   sudo mkdir -p /root/.ssh
   sudo chmod 0700 /root/.ssh
-  sops -d Development/nixos-configuration/metadata/backup_id_rsa | \
-          sudo tee /root/.ssh/backup_id_rsa >/dev/null
-  sudo chmod 0600 /root/.ssh/backup_id_rsa
+  sops -d Development/nixos-metadata/backup_id_ed25519 | \
+          sudo tee /root/.ssh/backup_id_ed25519 >/dev/null
+  sudo chmod 0600 /root/.ssh/backup_id_ed25519
+
+  sops -d Development/nixos-metadata/builder_id_ed25519 | \
+          sudo tee /root/.ssh/id_ed25519 >/dev/null
+  sudo chmod 0600 /root/.ssh/id_ed25519
 
   update-wifi-networks
   update-wireguard-keys
