86c6e06c0558f19206ebb6d93f20eec2c6339ba3
John Axel Eriksson <john@insane.se>
2020-07-23 16:19:54 +0200

    Update rust-analyzer and k3s

--- pkgs/k3s/metadata.nix
+++ pkgs/k3s/metadata.nix
@@ -1,1 +1,1 @@
-{ url = "https://github.com/rancher/k3s/releases/download/v1.18.4+k3s1/k3s"; version = "v1.18.4+k3s1"; sha256 = "140472asgahr4jkgmp4pkmkv96m5rspcbfnzy4xbpj3pvr6xc68c"; }
+{ url = "https://github.com/rancher/k3s/releases/download/v1.18.6+k3s1/k3s"; version = "v1.18.6+k3s1"; sha256 = "1r0f6285hk8iwc8mc7mcvgihgm6awllfi7f8xvf2bsdyrm5lvf0m"; }
--- pkgs/rust-analyzer-bin/metadata.nix
+++ pkgs/rust-analyzer-bin/metadata.nix
@@ -1,1 +1,1 @@
-{ url = "https://github.com/rust-analyzer/rust-analyzer/releases/download/2020-07-13/rust-analyzer-linux"; version = "2020-07-13"; sha256 = "060h846l8gwcyp745nnn7jhdkfp7gbrnfjpvalri1rprz8h65i57"; }
+{ url = "https://github.com/rust-analyzer/rust-analyzer/releases/download/2020-07-20/rust-analyzer-linux"; version = "2020-07-20"; sha256 = "08ydb2z268dn8bsy7qf993r93qny4wb0vq611nmny0lsfk8swj34"; }
