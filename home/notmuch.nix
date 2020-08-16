cf83ebf99c23c1dd9897be0b7e077764c81af786
John Axel Eriksson <john@insane.se>
2020-08-16 23:21:55 +0200

    Tweak notmuch initial filter some more

--- home/notmuch.nix
+++ home/notmuch.nix
@@ -10,7 +10,7 @@
 
       if list="$(notmuch show --format=raw "$msg" 2>/dev/null | grep -E '^List-ID:\s.+')"; then
         listname="$(echo "$list" | awk '{print $2}')"
-        notmuch tag +list/"$list" +thefeed -- "$msg"
+        notmuch tag -new +list/"$list" +thefeed -- "$msg"
       fi
 
     done
