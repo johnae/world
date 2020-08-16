49c3ec85dbc098752f81d0fd200bbb03f5bdaa01
John Axel Eriksson <john@insane.se>
2020-08-16 22:52:53 +0200

    Tweak notmuch initial filter

--- home/notmuch.nix
+++ home/notmuch.nix
@@ -2,37 +2,49 @@
 
 {
   programs.notmuch.enable = true;
+  programs.notmuch.new.tags = [ "new" ];
   programs.notmuch.hooks.postNew = ''
     maildir="${config.accounts.email.maildirBasePath}";
+
+    for msg in $(notmuch search --output=messages tag:new); do
+
+      if list="$(notmuch show --format=raw "$msg" 2>/dev/null | grep -E '^List-ID:\s.+')"; then
+        listname="$(echo "$list" | awk '{print $2}')"
+        notmuch tag +list/"$list" +thefeed -- "$msg"
+      fi
+
+    done
 
     touch "$maildir"/.notmuch/hooks/ledger.db
     while IFS= read -r line; do
         tag=$(echo "$line" | cut -d' ' -f1 -)
         entry=$(echo "$line" | cut -d' ' -f2 -)
         if [ -n "$entry" ]; then
-            notmuch tag +ledger/"$nm_tag" -inbox -- tag:inbox and tag:unread and from:"$entry"
+            notmuch tag +ledger/"$nm_tag" -new -- tag:new and from:"$entry"
         fi
     done < "$maildir"/.notmuch/hooks/ledger.db
 
     touch "$maildir"/.notmuch/hooks/spam.db
     for entry in $(cat "$maildir"/.notmuch/hooks/spam.db); do
         if [ -n "$entry" ]; then
-            notmuch tag +spam +deleted -inbox -unread -- tag:inbox and tag:unread and from:"$entry"
+            notmuch tag +spam +deleted -new -- tag:new and from:"$entry"
         fi
     done
 
     touch "$maildir"/.notmuch/hooks/thefeed.db
     for entry in $(cat "$maildir"/.notmuch/hooks/thefeed.db); do
         if [ -n "$entry" ]; then
-            notmuch tag +thefeed -inbox -- tag:inbox and tag:unread and from:"$entry"
+            notmuch tag +thefeed -new -- tag:new and from:"$entry"
         fi
     done
 
     touch "$maildir"/.notmuch/hooks/screened.db
     for entry in $(cat "$maildir"/.notmuch/hooks/screened.db); do
         if [ -n "$entry" ]; then
-            notmuch tag +screened -- tag:inbox and tag:unread and from:"$entry"
+            notmuch tag +screened -- tag:new and from:"$entry"
         fi
     done
+
+    notmuch tag +inbox +unread -new -- tag:new
   '';
 }
