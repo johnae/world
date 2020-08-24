0d14e078d1c11c1a1f72e9add11d45cbaeb8ad5e
buildkite-agent-linux-0 <root@buildkite-agent-linux-0>
2020-08-24 12:08:49 +0000

    Auto updated rust-analyzer

--- pkgs/rust-analyzer-bin/metadata.nix
+++ pkgs/rust-analyzer-bin/metadata.nix
@@ -1,1 +1,1 @@
-{ url = "https://github.com/rust-analyzer/rust-analyzer/releases/download/2020-08-17/rust-analyzer-linux"; version = "2020-08-17"; sha256 = "0wr20kapjqifx4p3lhpn34s4z7z859b6ywwhyxy7ix8vfdhgc0cx"; }
+{ url = "https://github.com/rust-analyzer/rust-analyzer/releases/download/2020-08-24/rust-analyzer-linux"; version = "2020-08-24"; sha256 = "06f28kw6wgjp8xv97l1dpvx9aczr5cnj5syzgz7wg439cxy4dbb0"; }
