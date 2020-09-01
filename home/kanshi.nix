603680a79f16d48a0fc3af8bde29ffaa69a6e8ab
John Axel Eriksson <john@insane.se>
2020-09-01 17:11:25 +0200

    Update kanshi config

--- home/kanshi.nix
+++ home/kanshi.nix
@@ -3,27 +3,33 @@
 {
   services.kanshi.enable = true;
   services.kanshi.profiles = {
-    work = {
-      outputs = [
-        {
-          criteria = "Dell Inc. DELL P2419H H9T0943";
-          mode = "1920x1080";
-          position = "0,0";
-          scale = 1.0;
-        }
-        {
-          criteria = "Sharp Corporation 0x14CC 0x00000000";
-          mode = "3840x2400";
-          position = "1920,0";
-          scale = 2.0;
-        }
-        {
-          criteria = "Dell Inc. DELL P2419H J9T0943";
-          mode = "1920x1080";
-          position = "3840,0";
-          scale = 1.0;
-        }
-      ];
-    };
+    default.outputs = [
+      {
+        criteria = "Sharp Corporation 0x14CC 0x00000000";
+        mode = "3840x2400";
+        position = "0,0";
+        scale = 2.0;
+      }
+    ];
+    work.outputs = [
+      {
+        criteria = "Dell Inc. DELL P2419H H9T0943";
+        mode = "1920x1080";
+        position = "0,0";
+        scale = 1.0;
+      }
+      {
+        criteria = "Sharp Corporation 0x14CC 0x00000000";
+        mode = "3840x2400";
+        position = "1920,0";
+        scale = 2.0;
+      }
+      {
+        criteria = "Dell Inc. DELL P2419H J9T0943";
+        mode = "1920x1080";
+        position = "3840,0";
+        scale = 1.0;
+      }
+    ];
   };
 }
