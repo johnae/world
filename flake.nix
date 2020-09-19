e4b437a2f0c844b4ab05e891b7c74f75ddc2e83f
John Axel Eriksson <john@insane.se>
2020-09-19 15:44:27 +0200

    home-manager: switch to using the nix-community repo

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home = {
-      url = "github:rycee/home-manager";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixpkgs-firefox-pipewire = {
