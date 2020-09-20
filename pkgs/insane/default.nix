6ed2e6f2075ceb7e7e1ccd196ece2eb207c617be
John Axel Eriksson <john@insane.se>
2020-09-20 22:45:28 +0200

    Remove pkgs/insane

--- pkgs/insane/default.nix
+++
@@ -1,6 +0,0 @@
-{ stdenv, lib, pkgs, ... }:
-rec {
-  insane-lib = pkgs.callPackage ./lib {};
-  buildkite-pipeline = pkgs.callPackage ./buildkite-pipeline {};
-  buildkite = pkgs.callPackage ./buildkite {};
-}
