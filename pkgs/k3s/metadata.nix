8ae6ab08b04e6481ab6d71d84f4391594643978c
buildkite-agent-linux-0 <root@buildkite-agent-linux-0>
2020-09-21 18:04:16 +0000

    Auto updated k3s

--- pkgs/k3s/metadata.nix
+++ pkgs/k3s/metadata.nix
@@ -1,1 +1,1 @@
-{ url = "https://github.com/rancher/k3s/releases/download/v1.19.1+k3s1/k3s"; version = "v1.19.1+k3s1"; sha256 = "1vrilb1mpfqbxi4r1vdixh3d7p276vjmksg6qrd4m9cr7yklnf1c"; }
+{ url = "https://github.com/rancher/k3s/releases/download/v1.19.2+k3s1/k3s"; version = "v1.19.2+k3s1"; sha256 = "0q1ay9i6gn3g33sbbqwbn7nh6zl3s1vssnzpkr3lamf4krd88b8x"; }
