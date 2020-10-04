083e09790c08f1b57db50d8be9db1e6279b0eb90
John Axel Eriksson <john@insane.se>
2020-10-04 23:26:29 +0200

    Update kanshi config

--- home/kanshi.nix
+++ home/kanshi.nix
@@ -13,16 +13,23 @@
     ];
     home.outputs = [
       {
+        criteria = "Ancor Communications Inc VG248 G2LMQS056758";
+        mode = "1920x1080";
+        position = "0,0";
+        scale = 1.0;
+        transform = "270";
+      }
+      {
         criteria = "Sharp Corporation 0x14CC 0x00000000";
         mode = "3840x2400";
-        position = "0,0";
+        position = "1080,0";
         scale = 2.0;
       }
       {
         criteria = "Unknown ASUS PB27U 0x0000C167";
         mode = "3840x2160";
-        position = "1920,0";
-        scale = 1.6;
+        position = "3000,0";
+        scale = 1.5;
       }
     ];
     work.outputs = [
