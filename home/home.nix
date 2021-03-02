75d9988ced048b75175385f78ccb178e6c335263
John Axel Eriksson <john@insane.se>
2021-03-02 17:36:17 +0100

    Add mimeapps

--- home/home.nix
+++ home/home.nix
@@ -80,6 +80,16 @@ in
   };
 
   xdg.enable = true;
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "text/html" = "firefox.desktop";
+      "x-scheme-handler/http" = "firefox.desktop";
+      "x-scheme-handler/https" = "firefox.desktop";
+      "x-scheme-handler/about" = "firefox.desktop";
+      "x-scheme-handler/unknown" = "firefox.desktop";
+    };
+  };
 
   home.file.".emacs".source =
     (pkgs.callPackage ../pkgs/my-emacs/config.nix { }).emacsConfig;
