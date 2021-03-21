b4f798e39f57cd23ac2bca56d3c577261f9bc26f
John Axel Eriksson <john@insane.se>
2021-03-21 21:04:01 +0100

    sway: remove emacs auto start

--- home/sway.nix
+++ home/sway.nix
@@ -287,9 +287,6 @@ in
           command = "${pkgs.dbus_tools}/bin/dbus-update-activation-environment WAYLAND_DISPLAY=$WAYLAND_DISPLAY";
         }
         {
-          command = "while [ ! -e ~/.emacs ]; do sleep 1; done; echo hello | ${pkgs.netcat}/bin/nc -w1 -U /var/run/user/1337/emacs/server";
-        }
-        {
           command = "${swayOnReload}/bin/sway-on-reload";
           always = true;
         }
