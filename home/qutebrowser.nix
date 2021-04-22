45c49893859abb383222ccae276eef7a93dd9a43
John Axel Eriksson <john@insane.se>
2021-04-22 22:11:48 +0200

    add qutebrowser config

---
+++ home/qutebrowser.nix
@@ -0,0 +1,17 @@
+{ config, pkgs, lib, ... }:
+
+{
+  programs.qutebrowser = {
+    enable = true;
+    settings = {
+      tabs.tabs_are_windows = true;
+      qt.args = [
+        "enable-native-gpu-memory-buffers"
+        "enable-gpu-rasterization"
+        "use-egl=desktop"
+        "ignore-gpu-blocklist"
+      ];
+      scrolling.smooth = true;
+    };
+  };
+}
