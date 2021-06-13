0eb2a298b0a620d257be9d94fd176d91a5bc4eb8
john <john@insane.se>
2021-06-14 00:01:19 +0200

    i3status-rust: update icons etc

--- users/profiles/i3status.nix
+++ users/profiles/i3status.nix
@@ -3,30 +3,34 @@
 {
   programs.i3status-rust = {
     enable = true;
-    bars.default.blocks = [
-      {
-        block = "net";
-        device = "wlan0";
-        format = "{ssid} {signal_strength} {ip} {speed_down;K*b}/{speed_up;K*b}";
-        interval = 5;
-      }
-      {
-        block = "cpu";
-        interval = 1;
-      }
-      {
-        block = "battery";
-        interval = 30;
-        format = "{percentage}% {time}";
-      }
-      {
-        block = "sound";
-      }
-      {
-        block = "time";
-        interval = 1;
-        format = "%b-%d %H:%M:%S";
-      }
-    ];
+    bars.default = {
+      theme = "modern";
+      icons = "awesome5";
+      blocks = [
+        {
+          block = "net";
+          device = "wlan0";
+          format = "{ssid} {signal_strength} {ip} {speed_down;K*b}/{speed_up;K*b}";
+          interval = 5;
+        }
+        {
+          block = "cpu";
+          interval = 1;
+        }
+        {
+          block = "battery";
+          interval = 30;
+          format = "{percentage}% {time}";
+        }
+        {
+          block = "sound";
+        }
+        {
+          block = "time";
+          interval = 1;
+          format = "%b-%d %H:%M:%S";
+        }
+      ];
+    };
   };
 }
