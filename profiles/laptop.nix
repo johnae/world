f257c63a91190c9e894f4541e127cbad4d9f56e0
john <john@insane.se>
2021-06-16 22:32:02 +0200

    Rearrange profiles a bit

--- profiles/laptop.nix
+++ profiles/laptop.nix
@@ -1,9 +1,17 @@
 { hostName, config, lib, pkgs, inputs, ... }:
 {
   imports = [
-    ./desktop.nix
+    ./workstation.nix
   ];
 
   services.logind.lidSwitch = "suspend-then-hibernate";
   services.disable-usb-wakeup.enable = true;
+  programs.light.enable = true;
+  services.upower.enable = true;
+
+  boot.kernel.sysctl = {
+    "vm.dirty_writeback_centisecs" = 1500;
+    "vm.laptop_mode" = 5;
+    "vm.swappiness" = 1;
+  };
 }
