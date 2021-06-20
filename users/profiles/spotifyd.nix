403d0476c0b9b782149ae53ac723ccce06300692
john <john@insane.se>
2021-06-20 22:28:29 +0200

    spotifyd: fix password input + hostName

--- users/profiles/spotifyd.nix
+++ users/profiles/spotifyd.nix
@@ -8,11 +8,11 @@ in
     settings = rec {
       global = rec {
         username = "binx";
-        password_cmd = "${pkgs.rbw}/bin/rbw get spotify.com ${username}";
+        password_cmd = "cat /run/secrets/spotifyd";
         backend = "pulseaudio";
         mixer = "PCM";
         volume-control = "alsa";
-        device_name = home.extraConfig.hostname;
+        device_name = home.extraConfig.hostName;
         bitrate = 320;
         cache_path = "/home/${home.username}/.cache/spotifyd";
         volume-normalisation = "true";
