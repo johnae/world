68f1d36023976f8dac0e6068773794760cc2ef2b
john <john@insane.se>
2021-06-20 22:27:53 +0200

    Use hosts.toml to automatically encrypt secrets to hosts

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,14 +1,28 @@
 let
-  inherit (builtins) mapAttrs fromTOML readFile;
-  hosts = mapAttrs (_: value: value.public_key)
-    (fromTOML (readFile ../hosts.toml));
-  johnae = "age1yubikey1qt7cjux5unxcsrw9dnkq8qsh0jgnwwvxzxm2jn2pxetjchtclmlk6xvpckq";
+  inherit (builtins) any baseNameOf filter
+    attrValues mapAttrs fromTOML readFile hasAttr;
+
+  hosts = mapAttrs (_: value: {
+    inherit (value) public_key;
+    secrets = if hasAttr "age" value then
+      attrValues (mapAttrs (_: s:
+        baseNameOf s.file
+      ) value.age.secrets)
+    else [];
+  })
+  (fromTOML (readFile ../hosts.toml));
+
+  hostKeys = secretFile: filter (v: v!=null)
+    (attrValues (mapAttrs (_: h:
+      if any (v: v == secretFile) h.secrets then
+        h.public_key
+      else null
+    ) hosts));
+
+  johnae = [ "age1yubikey1qt7cjux5unxcsrw9dnkq8qsh0jgnwwvxzxm2jn2pxetjchtclmlk6xvpckq" ];
+
 in
 {
-  "spotnix.age".publicKeys = [
-    hosts.eris
-    hosts.io
-    hosts.carbon
-    johnae
-  ];
+  "spotifyd.age".publicKeys = johnae ++ (hostKeys "spotifyd.age");
+  "spotnix.age".publicKeys = johnae ++ (hostKeys "spotnix.age");
 }
