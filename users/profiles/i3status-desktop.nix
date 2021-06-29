35bdb31de85ffd8b30050a9ccf9f244609ed3fb9
john <john@insane.se>
2021-06-29 12:56:38 +0200

    Use different i3status profiles for laptop/desktop machines

---
+++ users/profiles/i3status-desktop.nix
@@ -0,0 +1,31 @@
+{...}:
+
+{
+  programs.i3status-rust = {
+    enable = true;
+    bars.default = {
+      settings.theme.name = "modern";
+      icons = "awesome5";
+      blocks = [
+        {
+          block = "net";
+          device = "wlan0";
+          format = "{ssid} {signal_strength} {ip} {speed_down;K*b}/{speed_up;K*b}";
+          interval = 5;
+        }
+        {
+          block = "cpu";
+          interval = 1;
+        }
+        {
+          block = "sound";
+        }
+        {
+          block = "time";
+          interval = 1;
+          format = "%b-%d %H:%M:%S";
+        }
+      ];
+    };
+  };
+}
