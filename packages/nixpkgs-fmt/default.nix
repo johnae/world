babb996df30cb4a8ae4ba5301ea56def2cd5eb67
NixBot <johnae@users.noreply.github.com>
2021-07-02 03:28:52 +0000

    [Auto] update sub flake packages

--- packages/nixpkgs-fmt/default.nix
+++ packages/nixpkgs-fmt/default.nix
@@ -4,7 +4,7 @@ rustPlatform.buildRustPackage {
   version = inputs.nixpkgs-fmt.rev;
 
   src = inputs.nixpkgs-fmt;
-  cargoSha256 = "sha256-NdbOfHZCFDLvGiYpDL1JE/CxM8PvyNijsMWPdcgVTqc=";
+  cargoSha256 = "sha256-LE57k78cIQKmxiuU0rE7dFpRJnpM1mLZllN5z1Gn+OI=";
 
   doCheck = false;
 
