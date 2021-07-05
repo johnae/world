eeaa14c5d5a5769ff6297374228c7df2136e356a
john <john@insane.se>
2021-07-05 17:07:55 +0200

    WIP doesn't work

--- profiles/workstation.nix
+++ profiles/workstation.nix
@@ -61,31 +61,53 @@
 
   services.write-iwd-secrets.enable = true;
 
-  ## taken from NixOS wiki
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    media-session.config.alsa-monitor = {
+      rules = [
+        {
+          matches = [
+             { "node.name" = "~alsa_card.*"; }
+          ];
+          actions = {
+            update-props = {
+              "api.alsa.use-acp" = false;
+              #"api.alsa.use-ucm" = true;
+              #"api.acp.auto-port" = false;
+              #"api.acp.auto-profile" = false;
+            };
+          };
+        }
+        {
+          matches = [
+             { "node.name" = "~alsa_output.*"; }
+             { "node.name" = "~alsa_input.*"; }
+          ];
+          actions = {
+            update-props = {
+              "node.pause-on-idle" = false;
+            };
+          };
+        }
+      ];
+    };
     media-session.config.bluez-monitor.rules = [
       {
-        # Matches all cards
         matches = [ { "device.name" = "~bluez_card.*"; } ];
         actions = {
           "update-props" = {
             "bluez5.reconnect-profiles" = [ "hfp_hf" "hsp_hs" "a2dp_sink" ];
-            # mSBC is not expected to work on all headset + adapter combinations.
             "bluez5.msbc-support" = true;
-            # SBC-XQ is not expected to work on all headset + adapter combinations.
             "bluez5.sbc-xq-support" = true;
           };
         };
       }
       {
         matches = [
-          # Matches all sources
           { "node.name" = "~bluez_input.*"; }
-          # Matches all outputs
           { "node.name" = "~bluez_output.*"; }
         ];
         actions = {
