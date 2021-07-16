d1bbe78c4bae952d7edb7b3380997af1133ad2cc
John Axel Eriksson <john@insane.se>
2021-07-16 23:03:11 +0200

    emacs: remove jl-encrypt as I don't use it.

--- packages/my-emacs/default.nix
+++ packages/my-emacs/default.nix
@@ -14,24 +14,6 @@
 , makeWrapper
 , ...
 }:
-let
-  jl-encrypt = emacsPackages.melpaBuild {
-    pname = "jl-encrypt";
-    version = "20200904";
-
-    src = fetchgit {
-      url = "https://gitlab.com/lechten/defaultencrypt.git";
-      rev = "83fb2d46061127b51d65d3548ffbe18642398a52";
-      sha256 = "sha256-jtTXuHy2EsJroyt5PX+L3rJaX3xrdjcLnNpk3w9/470=";
-    };
-
-    recipe = writeText "jl-encrypt-recipe" ''
-      (jl-encrypt :fetcher git
-                  :url "https://gitlab.com/lechten/defaultencrypt.git"
-                  :files (:defaults))
-    '';
-  };
-in
 emacsWithPackagesFromUsePackage {
   alwaysEnsure = true;
   alwaysTangle = true;
@@ -46,5 +28,5 @@ emacsWithPackagesFromUsePackage {
           --prefix PATH : ${pkgs.lib.makeBinPath [ fish notmuch pandoc pass wl-clipboard kubectl kubectx google-cloud-sdk ]}
       '';
     });
-  extraEmacsPackages = epkgs: [ jl-encrypt epkgs.org-plus-contrib ];
+  extraEmacsPackages = epkgs: [ epkgs.org-plus-contrib ];
 }
