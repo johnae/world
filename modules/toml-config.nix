fd7044c26d414c2533e98e4acbe417640f98a20e
John Axel Eriksson <john@insane.se>
2021-08-05 15:30:22 +0200

    users are part of kvm group by default

--- modules/toml-config.nix
+++ modules/toml-config.nix
@@ -15,7 +15,7 @@ let
         else pkgs.bash;
         extraGroups = if hasAttr "extraGroups" value then
           value.extraGroups
-        else [ "wheel" "docker" "video" "audio" "plugdev" ];
+        else [ "wheel" "docker" "video" "audio" "plugdev" "kvm" ];
         isNormalUser = true;
       }
     );
