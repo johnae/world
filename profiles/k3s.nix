00c23a356d8a40e2e8814e7bb3d4d50298e0ff3b
John Axel Eriksson <john@insane.se>
2021-08-24 17:56:49 +0200

    k3s: open port 6443 on k3s nodes with server role

--- profiles/k3s.nix
+++ profiles/k3s.nix
@@ -1,9 +1,13 @@
 {lib, config, hostName,...}:
+let
+  cfg = config.services.k3s;
+in
 {
   services.k3s.enable = true;
   services.k3s.extraFlagsList = [
     "--node-label hostname=${hostName}"
   ];
+  networking.firewall.allowedTCPPorts = lib.mkIf (cfg.role == "server") [ 6443 ];
   environment.state."/keep" = {
     directories = [
       "/var/lib/dockershim"
