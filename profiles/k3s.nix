c79544df6e179bcc8eb49a2c1d4f8cda826c602d
John Axel Eriksson <john@insane.se>
2021-08-24 18:15:27 +0200

    k3s: skip writing kubeconfig to /

--- profiles/k3s.nix
+++ profiles/k3s.nix
@@ -6,7 +6,7 @@ in
   services.k3s.enable = true;
   services.k3s.extraFlagsList = [
     "--node-label hostname=${hostName}"
-  ] ++ (lib.optional (cfg.role == "server") "--write-kubeconfig /kubeconfig");
+  ];
   networking.firewall.allowedTCPPorts = lib.mkIf (cfg.role == "server") [ 6443 ];
   environment.state."/keep" = {
     directories = [
