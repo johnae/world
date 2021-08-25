7db0890d77458f8be1d342f96f29bef88a3a652a
John Axel Eriksson <john@insane.se>
2021-08-25 13:21:43 +0200

    k3s: actually skip deploying traefik etc, new way of doing so apparently

--- modules/k3s.nix
+++ modules/k3s.nix
@@ -3,11 +3,20 @@ let
   inherit (lib) mkOption mkIf mkMerge mkForce types;
   inherit (builtins) concatStringsSep;
   cfg = config.services.k3s;
+  k3sManifestsDir = "/var/lib/rancher/k3s/server/manifests";
 in
 {
   options.services.k3s.nodeID = mkOption {
     type = types.nullOr types.str;
     default = null;
+  };
+  options.services.k3s.autoDeployList = mkOption {
+    type = types.listOf types.path;
+    default = [];
+  };
+  options.services.k3s.skipDeployList = mkOption {
+    type = types.listOf types.str;
+    default = [];
   };
   options.services.k3s.extraFlagsList = mkOption {
     type = types.listOf types.str;
@@ -17,5 +26,15 @@ in
     systemd.enableUnifiedCgroupHierarchy = mkForce true;
     services.k3s.extraFlagsList = [ "--with-node-id ${cfg.nodeID}" ];
     services.k3s.extraFlags = concatStringsSep " " cfg.extraFlagsList;
+    systemd.services.k3s.preStart = ''
+    ${concatStringsSep "\n" (map (manifestName:
+      "touch ${k3sManifestsDir}/${manifestName}.skip"
+      ) cfg.skipDeployList)
+    }
+    ${concatStringsSep "\n" (map (manifestPath:
+      "cp ${manifestPath} ${k3sManifestsDir}/"
+      ) cfg.autoDeployList)
+    }
+    '';
   };
 }
--- profiles/k3s.nix
+++ profiles/k3s.nix
@@ -5,10 +5,8 @@ in
 {
   services.k3s.enable = true;
   services.k3s.package = pkgs.k3s-io;
-  services.k3s.extraFlagsList = [
-    "--node-label hostname=${hostName}"
-    "--no-deploy=traefik --no-deploy=servicelb --no-deploy=local-storage"
-  ];
+  services.k3s.extraFlagsList = [ "--node-label hostname=${hostName}" ];
+  services.k3s.skipDeployList = [ "traefik" "local-storage" ];
   networking.firewall.allowedTCPPorts = lib.mkIf (cfg.role == "server") [ 6443 ];
   environment.state."/keep" = {
     directories = [
