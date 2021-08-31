5d241d6e1443a64355ef4609d09961a33b90a77e
John Axel Eriksson <john@insane.se>
2021-08-31 23:39:36 +0200

    Add pueue + pueued service

---
+++ users/profiles/pueue.nix
@@ -0,0 +1,12 @@
+{pkgs, ...}:
+
+{
+  systemd.user.services.pueued = {
+    Unit.Description = "Pueue Daemon - CLI process scheduler and manager";
+    Service = {
+      Restart = "no";
+      ExecStart = "${pkgs.pueue}/bin/pueued -vv";
+    };
+    Install.WantedBy = [ "default.target" ];
+  };
+}
