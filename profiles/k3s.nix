d416bfbca9a9a856540fa79a142a9b681bbaa5eb
John Axel Eriksson <john@insane.se>
2021-09-25 17:40:01 +0200

    k3s: fix lint complaints

--- profiles/k3s.nix
+++ profiles/k3s.nix
@@ -1,7 +1,4 @@
-{lib, config, hostName, ...}:
-let
-  cfg = config.services.k3s;
-in
+{config, hostName, ...}:
 {
   services.k3s.enable = true;
   services.k3s.extraFlagsList = [
@@ -9,8 +6,6 @@ in
   ];
   services.k3s.disable = [ "traefik" "local-storage" ];
   services.k3s.disableNetworkPolicy = true;
-  #networking.firewall.allowedTCPPorts = lib.mkIf (cfg.role == "server") [ 6443 ];
-  #networking.firewall.allowedUDPPorts = [ 8472 ];
   networking.firewall.trustedInterfaces = [ "cni+" "flannel.1" "calico+" "cilium+" "lxc+" ];
   environment.state."/keep" = {
     directories = [
