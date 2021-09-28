c7169197dcef9b4b3a097962b88f251eaaed6e14
John Axel Eriksson <john@insane.se>
2021-09-28 19:22:34 +0200

    triton: try some things

--- hardware/yoga-slim-7-amd.nix
+++ hardware/yoga-slim-7-amd.nix
@@ -3,7 +3,7 @@
   imports = [
     ./amd.nix
     inputs.nixos-hardware.nixosModules.common-cpu-amd
-    inputs.nixos-hardware.nixosModules.common-gpu-amd
+    #inputs.nixos-hardware.nixosModules.common-gpu-amd
   ];
 }
 
