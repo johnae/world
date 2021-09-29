3322bf7d8b7a76673206319aba4930df2ce44d36
John Axel Eriksson <john@insane.se>
2021-09-29 11:12:07 +0200

    qutebrowser: add shortcuts to search engines

--- users/profiles/qutebrowser.nix
+++ users/profiles/qutebrowser.nix
@@ -1,6 +1,12 @@
 {
   programs.qutebrowser = {
     enable = true;
+    searchEngines = {
+      w = "https://en.wikipedia.org/wiki/Special:Search?search={}&go=Go&ns0=1";
+      aw = "https://wiki.archlinux.org/?search={}";
+      nw = "https://nixos.wiki/index.php?search={}";
+      g = "https://www.google.com/search?hl=en&q={}";
+    };
     settings = {
       tabs.tabs_are_windows = true;
       qt.args = [
