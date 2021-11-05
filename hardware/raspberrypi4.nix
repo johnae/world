fde2b4c2ad644a0cfd0057260b1c4dea45f5f271
John Axel Eriksson <john@insane.se>
2021-11-05 09:16:50 +0100

    raspberrypi4: move options from milkyway host to raspberrypi4 hardware profile

--- hardware/raspberrypi4.nix
+++ hardware/raspberrypi4.nix
@@ -10,4 +10,43 @@
     enable = true;
     version = 4;
   };
+
+  fileSystems."/".options = [ "defaults" "size=4G" "mode=755" ];
+
+  networking.usePredictableInterfaceNames = true;
+
+  boot.kernelParams = [
+    "8250.nr_uarts=1"
+    "console=ttyS0115200n8"
+    "console=ttyAMA0115200n8"
+    "cma=128M"
+    "console=tty1"
+    "boot.shell_on_fail"
+  ];
+
+  boot.initrd.luks.devices.cryptkey.keyFile = "/sys/firmware/devicetree/base/serial-number";
+
+  boot.initrd.availableKernelModules = [ "hid_generic" "usbhid" "uas" ];
+
+  boot.initrd.luks.cryptoModules = [
+    "aes"
+    "aes_generic"
+    "blowfish"
+    "twofish"
+    "serpent"
+    "cbc"
+    "xts"
+    "lrw"
+    "sha1"
+    "sha256"
+    "sha512"
+    "af_alg"
+    "algif_skcipher"
+    "xchacha12"
+    "xchacha20"
+    "adiantum"
+  ];
+
+  cryptsetup.luksFormat.extraParams = "--cipher xchacha12,aes-adiantum-plain64";
+
 }
