941ebfe50f1e5f50bc002e82aa3143e0cd355a1a
John Axel Eriksson <john@insane.se>
2021-11-05 09:05:17 +0100

    raspberrypi4: use the raspberrypi4 specific kernel

--- hardware/raspberrypi4.nix
+++ hardware/raspberrypi4.nix
@@ -1,10 +1,11 @@
-{inputs, lib, ...}:
+{inputs, pkgs, lib, ...}:
 {
   imports = [
     inputs.nixos-hardware.nixosModules.raspberry-pi-4
   ];
-  #boot.loader.systemd-boot.enable = lib.mkForce false;
   boot.loader.generic-extlinux-compatible.enable = false;
+  ## must use a specific kernel, the lastest doesn't work as of 2021-11-04
+  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_rpi4;
   boot.loader.raspberryPi = {
     enable = true;
     version = 4;
