1efdbb1df187f632007fde3d4855b0c600f3de8d
John Axel Eriksson <john@insane.se>
2021-11-06 23:39:25 +0100

    titan: fix unifi package def

--- profiles/unifi.nix
+++ profiles/unifi.nix
@@ -3,7 +3,7 @@
 {
   environment.state."/keep".directories = [ "/var/lib/unifi" ];
   services.unifi.enable = true;
-  services.unifi.package = pkgs.unifi;
+  services.unifi.unifiPackage = pkgs.unifi;
   systemd.services.unifi.unitConfig.RequiresMountsFor = "/var/lib/unifi";
   networking.firewall.allowedTCPPorts = [ 8443 ];
   users.users.unifi.group = "unifi";
