18233128974643cbc82f24e9d022cd61189a2774
John Axel Eriksson <john@insane.se>
2021-11-09 00:05:08 +0100

    unifi: add port 8443 in firewall as allowed tcp port

--- profiles/unifi.nix
+++ profiles/unifi.nix
@@ -6,6 +6,7 @@
   services.unifi.unifiPackage = pkgs.unifi;
   services.unifi.openPorts = true;
   systemd.services.unifi.unitConfig.RequiresMountsFor = "/var/lib/unifi";
+  networking.firewall.allowedTCPPorts = [ 8443 ];
   users.users.unifi.group = "unifi";
   users.users.unifi.isSystemUser = true;
   users.groups.unifi = {};
