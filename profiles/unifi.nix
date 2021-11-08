cb01a5048f614c6ffae803d4064fd6975a9b7a05
John Axel Eriksson <john@insane.se>
2021-11-08 23:06:58 +0100

    polaris: ensure unifi openPorts is set

--- profiles/unifi.nix
+++ profiles/unifi.nix
@@ -4,8 +4,8 @@
   environment.state."/keep".directories = [ "/var/lib/unifi" ];
   services.unifi.enable = true;
   services.unifi.unifiPackage = pkgs.unifi;
+  services.unifi.openPorts = true;
   systemd.services.unifi.unitConfig.RequiresMountsFor = "/var/lib/unifi";
-  networking.firewall.allowedTCPPorts = [ 8443 ];
   users.users.unifi.group = "unifi";
   users.users.unifi.isSystemUser = true;
   users.groups.unifi = {};
