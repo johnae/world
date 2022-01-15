875a7f03a65ea2762888140a133ccd1d66666a51
John Axel Eriksson <john@insane.se>
2022-01-15 14:46:52 +0100

    unifi on polaris: use later mongodb because of pyyaml 6.0 / python2 / scons

--- profiles/unifi.nix
+++ profiles/unifi.nix
@@ -4,6 +4,7 @@
   environment.state."/keep".directories = [ "/var/lib/unifi" ];
   services.unifi.enable = true;
   services.unifi.unifiPackage = pkgs.unifi;
+  services.unifi.mongodbPackage = pkgs.mongodb-4_2;
   services.unifi.openFirewall = true;
   systemd.services.unifi.unitConfig.RequiresMountsFor = "/var/lib/unifi";
   networking.firewall.allowedTCPPorts = [ 8443 ];
