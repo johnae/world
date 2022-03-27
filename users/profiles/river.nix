99cebf0d41a434bb4a3b5fedfcac61a62c2fdd9a
John Axel Eriksson <john@insane.se>
2022-03-27 23:50:48 +0200

    river: update keybindings

--- users/profiles/river.nix
+++ users/profiles/river.nix
@@ -50,78 +50,86 @@ in {
   wayland.windowManager.river.settings = {
     inherit input;
 
-    map.normal."Super Return" = "spawn alacritty";
-    map.normal."Super D" = "spawn 'rofi -show drun'";
-    map.normal."Super Q" = "close";
-    map.normal."Super+Shift Q" = "exit";
+    map.normal.Super.Return = "spawn alacritty";
+    map.normal.Super.D = "spawn 'rofi -show drun'";
+    map.normal.Super.Q = "close";
 
-    map.normal."Super+Shift E" = "spawn 'emacsclient -c -n -a='";
+    map.normal."Super+Shift".E = "spawn 'emacsclient -c -n -a='";
 
-    map.normal."Super J" = "focus-view next";
-    map.normal."Super K" = "focus-view previous";
+    map.normal.Super.J = "focus-view next";
+    map.normal.Super.K = "focus-view previous";
 
-    map.normal."Super Left" = "focus-view next";
-    map.normal."Super Right" = "focus-view previous";
+    map.normal.Super.Left = "focus-view next";
+    map.normal.Super.Right = "focus-view previous";
 
-    map.normal."Super+Shift J" = "swap next";
-    map.normal."Super+Shift K" = "swap previous";
+    map.normal."Super+Shift".J = "swap next";
+    map.normal."Super+Shift".K = "swap previous";
 
-    map.normal."Super+Control J" = ["focus-view next" "zoom"];
-    map.normal."Super+Control K" = ["focus-view previous" "zoom"];
+    map.normal."Super+Control".J = ["focus-view next" "zoom"];
+    map.normal."Super+Control".K = ["focus-view previous" "zoom"];
 
-    map.normal."Super Minus" = "spawn 'rofi-rbw'";
-    map.normal."Super+Shift Minus" = "spawn 'passonly=y rofi-rbw'";
+    map.normal.Super.Minus = "spawn 'rofi-rbw'";
+    map.normal."Super+Shift".Minus = "spawn 'passonly=y rofi-rbw'";
 
-    map.normal."Super T" = "spawn 'rofi-spotify-search track'";
-    map.normal."Super P" = "spawn 'rofi-spotify-search playlist'";
-    map.normal."Super+Shift N" = "spawn 'spotify-cmd next'";
-    map.normal."Super+Shift P" = "spawn 'spotify-cmd prev'";
-    map.normal."Super+Shift M" = "spawn 'spotify-cmd pause'";
+    map.normal.Super.T = "spawn 'rofi-spotify-search track'";
+    map.normal.Super.P = "spawn 'rofi-spotify-search playlist'";
+    map.normal."Super+Shift".N = "spawn 'spotify-cmd next'";
+    map.normal."Super+Shift".P = "spawn 'spotify-cmd prev'";
+    map.normal."Super+Shift".M = "spawn 'spotify-cmd pause'";
 
-    map.normal."Super+Shift R" = "spawn 'systemctl --user restart graphical-session.target'";
+    map.normal."Super+Shift".R = "spawn 'systemctl --user restart graphical-session.target'";
 
-    map.normal."Super Period" = "focus-output next";
-    map.normal."Super Comma" = "focus-output previous";
+    map.normal.Super.Period = "focus-output next";
+    map.normal.Super.Comma = "focus-output previous";
 
-    map.normal."Super+Shift Period" = "send-to-output next";
-    map.normal."Super+Shift Comma" = "send-to-output previous";
+    map.normal."Super+Shift".Period = "send-to-output next";
+    map.normal."Super+Shift".Comma = "send-to-output previous";
 
-    map.normal."Super Space" = "zoom";
+    map.normal.Super.Space = "zoom";
 
-    map.normal."Super+Alt H" = "move left 100";
-    map.normal."Super+Alt J" = "move down 100";
-    map.normal."Super+Alt K" = "move up 100";
-    map.normal."Super+Alt L" = "move right 100";
+    map.normal."Super+Alt".H = "move left 100";
+    map.normal."Super+Alt".J = "move down 100";
+    map.normal."Super+Alt".K = "move up 100";
+    map.normal."Super+Alt".L = "move right 100";
 
-    map.normal."Super+Alt+Control H" = "snap left";
-    map.normal."Super+Alt+Control J" = "snap down";
-    map.normal."Super+Alt+Control K" = "snap up";
-    map.normal."Super+Alt+Control L" = "snap right";
-    map.normal."Super+Alt+Shift H" = "resize horizontal -100";
-    map.normal."Super+Alt+Shift J" = "resize vertical 100";
-    map.normal."Super+Alt+Shift K" = "resize vertical -100";
-    map.normal."Super+Alt+Shift L" = "resize horizontal 100";
+    map.normal."Super+Alt+Control".H = "snap left";
+    map.normal."Super+Alt+Control".J = "snap down";
+    map.normal."Super+Alt+Control".K = "snap up";
+    map.normal."Super+Alt+Control".L = "snap right";
+    map.normal."Super+Alt+Shift".H = "resize horizontal -100";
+    map.normal."Super+Alt+Shift".J = "resize vertical 100";
+    map.normal."Super+Alt+Shift".K = "resize vertical -100";
+    map.normal."Super+Alt+Shift".L = "resize horizontal 100";
 
-    map-pointer.normal."Super BTN_LEFT" = "move-view";
-    map-pointer.normal."Super BTN_RIGHT" = "resize-view";
+    map-pointer.normal.Super.BTN_LEFT = "move-view";
+    map-pointer.normal.Super.BTN_RIGHT = "resize-view";
 
-    map.normal."Super+Control Space" = "toggle-float";
-    map.normal."Super F" = "toggle-fullscreen";
+    map.normal."Super+Control".Space = "toggle-float";
+    map.normal.Super.F = "toggle-fullscreen";
 
     ### kile layouts ###
 
-    map.normal."Super+Control Up" = ''send-layout-cmd kile "focused U ((h: v v) 1 0.65 0)"'';
-    map.normal."Super+Control Down" = ''send-layout-cmd kile "focused D ((h: v v) 1 0.65 1)"'';
-    map.normal."Super+Control Left" = ''send-layout-cmd kile "focused L ((v: h h) 1 0.65 0)"'';
-    map.normal."Super+Control Right" = ''send-layout-cmd kile "focused R ((v: h h) 1 0.65 1)"'';
-    map.normal."Super+Control D" = ''send-layout-cmd kile "focused Deck deck"'';
-    map.normal."Super+Control F" = ''send-layout-cmd kile "focused Full full"'';
+    map.normal."Super+Control".Up = ''send-layout-cmd kile "focused U ((h: v v) 1 0.65 0)"'';
+    map.normal."Super+Control".Down = ''send-layout-cmd kile "focused D ((h: v v) 1 0.65 1)"'';
+    map.normal."Super+Control".Left = ''send-layout-cmd kile "focused L ((v: h h) 1 0.65 0)"'';
+    map.normal."Super+Control".Right = ''send-layout-cmd kile "focused R ((v: h h) 1 0.65 1)"'';
+    map.normal."Super+Control".D = ''send-layout-cmd kile "focused Deck deck"'';
+    map.normal."Super+Control".F = ''send-layout-cmd kile "focused Full full"'';
 
     ####################
 
-    additional-modes = ["passthrough"];
-    map.normal."Super F11" = "enter-mode passthrough";
-    map.passthrough."Super F11" = "enter-mode normal";
+    additional-modes = ["passthrough" "wmctl"];
+
+    map.normal.Super.F11 = "enter-mode passthrough";
+    map.passthrough.Super.F11 = "enter-mode normal";
+
+    map.normal.Super.Escape = "enter-mode wmctl";
+    map.wmctl.Super.Escape = "enter-mode normal";
+    map.wmctl.Super.P = ["enter-mode normal" "systemctl poweroff"];
+    map.wmctl.Super.H = ["enter-mode normal" "systemctl hibernate"];
+    map.wmctl.Super.R = ["enter-mode normal" "systemctl reboot"];
+    map.wmctl.Super.S = ["enter-mode normal" "systemctl suspend-then-hibernate"];
+    map.wmctl.Super.L = ["enter-mode normal" "exit"];
 
     background-color = "0x002b36";
     border-color-focused = "0x93a1a1";
