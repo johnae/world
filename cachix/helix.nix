d899b3b15152fc03e5a786e03aace7afa82350ec
John Axel Eriksson <john@insane.se>
2022-04-03 16:24:16 +0200

    cachix: add helix cache

---
+++ cachix/helix.nix
@@ -0,0 +1,10 @@
+{
+  nix.settings = {
+    substituters = [
+      "https://helix.cachix.org"
+    ];
+    trusted-public-keys = [
+      "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
+    ];
+  };
+}
