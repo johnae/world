a67ae16d3e67859f736154124c0a4faecb478b03
John Axel Eriksson <john@insane.se>
2022-05-14 17:15:49 +0200

    emacs: add org-roam updater

--- users/profiles/emacs.nix
+++ users/profiles/emacs.nix
@@ -1,19 +1,14 @@
 {
   config,
   pkgs,
+  lib,
   ...
 }: let
   inherit (config) home;
-in {
-  services.emacs = {
-    enable = true;
-    package = pkgs.my-emacs;
-    socketActivation.enable = true;
-  };
-
-  systemd.user.services.org-agenda-sync = let
-    agenda-sync = pkgs.writeStrictShellScriptBin "agenda-sync" ''
-      ssh_identity="/run/agenix/id_ed25519_agenda_updater"
+  inherit (lib) mkMerge;
+  repo-sync = name: key: let
+    sync-script = pkgs.writeStrictShellScriptBin "repo-sync-${name}" ''
+      ssh_identity="/run/agenix/${key}"
       if [ ! -e "$ssh_identity" ]; then
         echo "$ssh_identity" missing
         exit 1
@@ -21,14 +16,14 @@ in {
       GIT_SSH_COMMAND="ssh -i $ssh_identity -o IdentitiesOnly=yes"
       export GIT_SSH_COMMAND
 
-      if [ ! -d ~/Development/org-agenda ]; then
+      if [ ! -d ~/Development/${name} ]; then
         cd ~/Development
-        git clone git@github.com:johnae/org-agenda
+        git clone git@github.com:johnae/${name}
         cd ~
       fi
 
-      echo Starting agenda git sync
-      cd ~/Development/org-agenda
+      echo Starting ${name} git sync
+      cd ~/Development/${name}
       git add .
       git commit -m "Auto-commit" || true
       echo Pulling changes from remote
@@ -38,25 +33,42 @@ in {
       git status
     '';
   in {
-    Unit.Description = "Continuously git commit push/pull org agenda";
-    Service.ExecStart = "${agenda-sync}/bin/agenda-sync";
-  };
-
-  systemd.user.paths.org-agenda-sync = {
-    Unit.Description = "Continuously git commit push/pull org agenda";
-    Path = {
-      PathChanged = "/home/${home.username}/Development/org-agenda";
-      Unit = "org-agenda-sync.service";
+    systemd.user.services."${name}-sync" = {
+      Unit.Description = "Continuously git commit push/pull ${name}";
+      Service.ExecStart = "${sync-script}/bin/repo-sync-${name}";
     };
-    Install.WantedBy = ["paths.target"];
-  };
 
-  systemd.user.timers.org-agenda-sync = {
-    Unit.Description = "Continuously git commit push/pull org agenda";
-    Timer = {
-      OnCalendar = "*:0/5";
-      Unit = "org-agenda-sync.service";
+    systemd.user.paths."${name}-sync" = {
+      Unit.Description = "Continuously git commit push/pull ${name}";
+      Path = {
+        PathChanged = "/home/${home.username}/Development/${name}";
+        Unit = "${name}-sync.service";
+      };
+      Install.WantedBy = ["paths.target"];
     };
-    Install.WantedBy = ["timers.target"];
+
+    systemd.user.timers."${name}-sync" = {
+      Unit.Description = "Continuously git commit push/pull ${name}";
+      Timer = {
+        OnCalendar = "*:0/5";
+        Unit = "${name}-sync.service";
+      };
+      Install.WantedBy = ["timers.target"];
+    };
   };
-}
+in
+  mkMerge [
+    {
+      services.emacs = {
+        enable = true;
+        package = pkgs.my-emacs;
+        socketActivation.enable = true;
+      };
+    }
+    #(
+    #  repo-sync "org-agenda" "id_ed25519_agenda_updater"
+    #)
+    (
+      repo-sync "org-roam" "id_ed25519_roam_updater"
+    )
+  ]
