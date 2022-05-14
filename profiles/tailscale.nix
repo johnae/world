08d9291b871f578dee69567cbf11a422723f64fe
John Axel Eriksson <john@insane.se>
2022-05-14 11:44:20 +0200

    Fix a potential issue with tailscale + firewall/check reverse path

--- profiles/tailscale.nix
+++ profiles/tailscale.nix
@@ -1,4 +1,6 @@
 {
   services.tailscale.enable = true;
   networking.firewall.trustedInterfaces = ["tailscale0"];
+  # Strict reverse path filtering breaks Tailscale exit node use and some subnet routing setupsStrict reverse path filtering breaks Tailscale exit node use and some subnet routing setups, see: https://github.com/tailscale/tailscale/issues/4432
+  networking.firewall.checkReversePath = "loose";
 }
