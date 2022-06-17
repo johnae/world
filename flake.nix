fc5362c60f7a4739cc97ba808392b192fa0f38ad
John Axel Eriksson <john@insane.se>
2022-06-17 22:36:17 +0200

    remove mu4e-dashboard flake input

--- flake.nix
+++ flake.nix
@@ -165,10 +165,6 @@
       url = "https://github.com/fluxcd/flux2/releases/download/v0.31.1/install.yaml"; # gh-release-update
       flake = false;
     };
-    mu4e-dashboard = {
-      url = "github:rougier/mu4e-dashboard";
-      flake = false;
-    };
   };
 
   outputs = {
