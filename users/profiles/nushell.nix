769d9d7fbb021097d00bfa849a2a0cd9ce76fe90
John Axel Eriksson <john@insane.se>
2022-08-17 13:13:18 +0200

    Remove unused file

--- users/profiles/nushell.nix
+++
@@ -1,9 +0,0 @@
-{pkgs, ...}: {
-  programs.nushell = {
-    enable = true;
-    package = pkgs.nu;
-    configFile.source = ./nushell/config.nu;
-    envFile.source = ./nushell/env.nu;
-  };
-  xdg.configFile."nushell/starship.nu".source = ./nushell/starship.nu;
-}
