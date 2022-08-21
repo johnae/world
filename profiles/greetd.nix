d7efba416d04804824ce6773a49c58071f43d7d3
John Axel Eriksson <john@insane.se>
2022-08-21 16:55:04 +0200

    greetd/tuigreet: prevent systemd from spewing greeter with log msgs

--- profiles/greetd.nix
+++ profiles/greetd.nix
@@ -6,4 +6,9 @@
       default_session.command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd sway";
     };
   };
+  ## prevents systemd spewing the console with log messages when greeter is active
+  systemd.services.greetd.serviceConfig = {
+    ExecStartPre = "${pkgs.util-linux}/bin/kill -SIGRTMIN+21 1";
+    ExecStopPost = "${pkgs.util-linux}/bin/kill -SIGRTMIN+20 1";
+  };
 }
