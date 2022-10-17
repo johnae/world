11b4d49304438e1606f5a2deea46e709e61d4520
John Axel Eriksson <john@insane.se>
2022-10-17 19:32:51 +0200

    fix speling

--- profiles/hardware/raspberrypi4.nix
+++ profiles/hardware/raspberrypi4.nix
@@ -8,7 +8,7 @@
     inputs.nixos-hardware.nixosModules.raspberry-pi-4
   ];
   boot.loader.generic-extlinux-compatible.enable = false;
-  ## must use a specific kernel, the lastest doesn't work as of 2021-11-04
+  ## must use a specific kernel, the latest doesn't work as of 2021-11-04
   boot.kernelPackages = lib.mkForce pkgs.linuxPackages_rpi4;
   boot.loader.systemd-boot.enable = lib.mkForce false;
   boot.loader.raspberryPi = {
