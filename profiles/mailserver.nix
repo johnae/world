c7f7c15b68fb40fd2d2781ff98495ab16481b325
John Axel Eriksson <john@insane.se>
2022-10-23 23:53:47 +0200

    mailserver tweaks

--- profiles/mailserver.nix
+++ profiles/mailserver.nix
@@ -5,29 +5,32 @@
 }: let
   cfg = config.mailserver;
 in {
-  ## we want to use dns for certs, not http
-  services.nginx.enable = lib.mkForce false;
-  mailserver.openFirewall = false; ## disable this explicitly so we can remove port 80 from default open ports below
-  networking.firewall = with cfg;
+  config = with cfg;
     lib.mkIf enable {
-      allowedTCPPorts =
-        [25]
-        ++ lib.optional enableSubmission 587
-        ++ lib.optional enableSubmissionSsl 465
-        ++ lib.optional enableImap 143
-        ++ lib.optional enableImapSsl 993
-        ++ lib.optional enablePop3 110
-        ++ lib.optional enablePop3Ssl 995
-        ++ lib.optional enableManageSieve 4190;
+      ## we want to use dns for certs, not http
+      services.nginx.enable = lib.mkForce false;
+      mailserver.openFirewall = false; ## disable this explicitly so we can remove port 80 from default open ports below
+      networking.firewall = with cfg;
+        lib.mkIf enable {
+          allowedTCPPorts =
+            [25]
+            ++ lib.optional enableSubmission 587
+            ++ lib.optional enableSubmissionSsl 465
+            ++ lib.optional enableImap 143
+            ++ lib.optional enableImapSsl 993
+            ++ lib.optional enablePop3 110
+            ++ lib.optional enablePop3Ssl 995
+            ++ lib.optional enableManageSieve 4190;
+        };
+      environment.state."/keep".directories = [
+        "/var/vmail"
+        "/var/dkim"
+        "/var/sieve"
+        "/var/certs"
+        "/var/lib/postfix"
+        "/var/lib/dhparams"
+        "/var/lib/acme"
+        "/var/lib/dovecot"
+      ];
     };
-  environment.state."/keep".directories = [
-    "/var/vmail"
-    "/var/dkim"
-    "/var/sieve"
-    "/var/certs"
-    "/var/lib/postfix"
-    "/var/lib/dhparams"
-    "/var/lib/acme"
-    "/var/lib/dovecot"
-  ];
 }
