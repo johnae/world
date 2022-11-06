dec08ef6b8b03e1dda2e810a355674eb838aca88
John Axel Eriksson <john@insane.se>
2022-11-06 23:00:24 +0100

    k3s: use unified cgroup hierarchy again

--- profiles/k3s.nix
+++ profiles/k3s.nix
@@ -8,7 +8,6 @@
   inherit (lib) optionals mkIf;
   cfg = config.services.k3s;
 in {
-  systemd.enableUnifiedCgroupHierarchy = false;
   services.k3s.settings.node-label.hostname = hostName;
   services.k3s.disable = ["traefik"];
 
