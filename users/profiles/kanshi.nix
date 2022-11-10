fb71ac6388889ad5d782cf4f5796e4f089f0c04c
John Axel Eriksson <john@insane.se>
2022-11-10 12:30:27 +0100

    kanshi: update config

--- users/profiles/kanshi.nix
+++ users/profiles/kanshi.nix
@@ -12,14 +12,14 @@
     ];
     desktop.outputs = [
       {
-        criteria = "Goldstar Company Ltd LG HDR 4K 0x0000AF76";
+        criteria = "LG Electronics LG HDR 4K 0x0000AF76";
         #criteria = "DP-2";
         mode = "3840x2160";
         position = "0,0";
         scale = 1.0;
       }
       {
-        criteria = "Unknown ASUS PB27U 0x0000C167";
+        criteria = "ASUSTek COMPUTER INC ASUS PB27U 0x0000C167";
         #criteria = "DP-1";
         mode = "3840x2160";
         position = "3840,0";
