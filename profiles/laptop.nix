acf1a4bb0d3b02dd8ef1f2eae82c8f92c43a007c
John Axel Eriksson <john@insane.se>
2022-11-11 09:31:19 +0100

    laptops: enable tlp

--- profiles/laptop.nix
+++ profiles/laptop.nix
@@ -7,6 +7,10 @@
   services.disable-usb-wakeup.enable = true;
   programs.light.enable = true;
   services.upower.enable = true;
+  services.tlp.enable = true;
+  services.tlp.settings = {
+    USB_AUTOSUSPEND = 0;
+  };
 
   boot.kernel.sysctl = {
     "vm.dirty_writeback_centisecs" = lib.mkDefault 1500;
