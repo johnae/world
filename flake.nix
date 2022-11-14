e817a5f53ca853a895bf445bb4230b784da5cd7f
NixBot <johnae@users.noreply.github.com>
2022-11-14 01:55:08 +0000

    [Auto] update all inputs

--- flake.nix
+++ flake.nix
@@ -74,7 +74,7 @@
     notracking.url = "github:notracking/hosts-blocklists";
     nur.url = "github:nix-community/NUR";
     nushell.flake = false;
-    nushell.url = "github:nushell/nushell/0.70.0"; # gh-release-update
+    nushell.url = "github:nushell/nushell/0.71.0"; # gh-release-update
     persway.inputs.devshell.follows = "devshell";
     persway.inputs.dream2nix.follows = "dream2nix";
     persway.inputs.fenix.follows = "fenix";
