c5cf374fdd8982419361f339ae0219c3bdf9113d
John Axel Eriksson <john@insane.se>
2022-12-10 15:02:22 +0100

    emacs: fix package name emacsNativeComp -> emacsUnstable

--- packages/my-emacs/default.nix
+++ packages/my-emacs/default.nix
@@ -14,17 +14,17 @@
   inputs,
   lib,
   makeWrapper,
-  emacsPkg ? pkgs.emacsNativeComp.override { 
-              nativeComp = true;
-              withNS = false;
-              withX = false;
-              withGTK2 = false;
-              withGTK3 = false;
-              withWebP = false;
-  },
+  emacsPkg ?
+    pkgs.emacsUnstable.override {
+      nativeComp = true;
+      withNS = false;
+      withX = false;
+      withGTK2 = false;
+      withGTK3 = false;
+      withWebP = false;
+    },
   ...
 }:
-
 emacsWithPackagesFromUsePackage {
   alwaysEnsure = true;
   alwaysTangle = true;
