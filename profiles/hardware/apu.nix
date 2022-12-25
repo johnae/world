af21b9e1741377d2d8489a2cd9a838d8be3bfb2b
John Axel Eriksson <john@insane.se>
2022-12-26 00:08:25 +0100

    apu: fix serial console

--- profiles/hardware/apu.nix
+++ profiles/hardware/apu.nix
@@ -11,10 +11,13 @@
   boot.loader.grub.version = 2;
   boot.loader.grub.device = "/dev/sda";
 
-  boot.kernelParams = [
-    "console=ttyS0,115200"
-    "console=tty1"
-  ];
+  boot.kernelParams = ["console=ttyS0,115200n8"];
+
+  boot.loader.grub.extraConfig = "
+    serial --speed=115200 --unit=0 --word=8 --parity=no --stop=1
+    terminal_input serial
+    terminal_output serial
+  ";
 
   hardware.cpu.amd.updateMicrocode = true;
 }
