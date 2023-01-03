4bf2b0496b2de0604a56b16d45f2a57df2aa7cce
Machine <37350196+53f463c7@users.noreply.github.com>
2023-01-03 07:20:27 +0100

    [Auto] Package updates (#428)
    * [Auto] update all inputs
    
    * Fix issues with dream2nix + fix for home-manager
    
    * Remove host triton
    
    Co-authored-by: NixBot <johnae@users.noreply.github.com>
    Co-authored-by: John Axel Eriksson <john@insane.se>

--- users/profiles/sway.nix
+++ users/profiles/sway.nix
@@ -145,7 +145,7 @@ in {
     MOZ_ENABLE_WAYLAND = "1";
     MOZ_USE_XINPUT2 = "1";
     XCURSOR_THEME = xcursor_theme;
-    QT_STYLE_OVERRIDE = "gtk";
+    QT_STYLE_OVERRIDE = lib.mkForce "gtk";
     _JAVA_AWT_WM_NONREPARENTING = "1";
     NIXOS_OZONE_WL = "1";
   };
