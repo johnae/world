5db44081ebea82182bf3fa14aacc8713182a2a4c
John Axel Eriksson <john@insane.se>
2023-04-05 09:45:57 +0200

    Remove media-session, deprecated in favor of wireplumber

--- profiles/workstation.nix
+++ profiles/workstation.nix
@@ -87,33 +87,6 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    media-session.config.bluez-monitor.rules = [
-      {
-        # Matches all cards
-        matches = [{"device.name" = "~bluez_card.*";}];
-        actions = {
-          "update-props" = {
-            "bluez5.auto-connect" = ["hfp_hf" "hsp_hs" "a2dp_sink"];
-            "bluez5.reconnect-profiles" = ["hfp_hf" "hsp_hs" "a2dp_sink"];
-            # mSBC is not expected to work on all headset + adapter combinations.
-            "bluez5.msbc-support" = true;
-            # SBC-XQ is not expected to work on all headset + adapter combinations.
-            "bluez5.sbc-xq-support" = true;
-          };
-        };
-      }
-      {
-        matches = [
-          # Matches all sources
-          {"node.name" = "~bluez_input.*";}
-          # Matches all outputs
-          {"node.name" = "~bluez_output.*";}
-        ];
-        actions = {
-          "node.pause-on-idle" = false;
-        };
-      }
-    ];
   };
   xdg.portal.enable = true;
   xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-wlr pkgs.xdg-desktop-portal-gtk];
