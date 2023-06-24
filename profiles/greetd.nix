608c42b203e3a0643c19483945d9279540742c89
John Axel Eriksson <john@insane.se>
2023-06-24 23:43:02 +0200

    WIP: use regreet greeter

--- profiles/greetd.nix
+++ profiles/greetd.nix
@@ -69,32 +69,31 @@
     cmd = "${pkgs.river}/bin/river";
   };
 
-  desktopSession = name: command:
-    pkgs.writeText "${name}.desktop" ''
-      [Desktop Entry]
-      Type=Application
-      Name=${name}
-      Exec=${command}
-    '';
+  desktopSession = name: command: ''
+    [Desktop Entry]
+    Type=Application
+    Name=${name}
+    Exec=${command}
+  '';
 
   sessions = [
     {
-      name = "sway.desktop";
-      path = desktopSession "sway" "${runSway}/bin/sway";
+      name = "sway";
+      value = desktopSession "Sway" "${runSway}/bin/sway";
     }
     {
-      name = "river.desktop";
-      path = desktopSession "river" "${runRiver}/bin/river";
+      name = "river";
+      value = desktopSession "River" "${runRiver}/bin/river";
     }
     {
-      name = "nushell.desktop";
-      path = desktopSession "nushell" "${pkgs.nushell}/bin/nu";
+      name = "nushell";
+      value = desktopSession "NuShell" "${pkgs.nushell}/bin/nu";
     }
   ];
 
-  createGreeter = default: sessions: let
+  createTuiGreeter = default: sessions: let
     sessionDir = pkgs.linkFarm "sessions" (
-      builtins.filter (item: item.name != "${default}.desktop") sessions
+      map (session: pkgs.writeText "${session.name}" session.value) (builtins.filter (item: item.name != default) sessions)
     );
   in
     pkgs.writeShellApplication {
@@ -104,12 +103,68 @@
         tuigreet --sessions ${sessionDir} --time -r --remember-session --power-shutdown 'systemctl poweroff' --power-reboot 'systemctl reboot' --cmd ${default}
       '';
     };
+
+  regreetBg = "${../files/forest.jpg}";
+
+  createRegreetGreeter = sessions: let
+    waylandSessions = map (session: pkgs.writeTextDir "wayland-sessions/${session.name}.desktop" session.value) sessions;
+    sessionDir = pkgs.symlinkJoin {
+      name = "sessions";
+      paths = [waylandSessions];
+    };
+    swayConfig = pkgs.writeText "sway.config.regreet" ''
+      output "*" {
+      bg ${regreetBg} fill
+      }
+      exec "XDG_DATA_DIRS=${sessionDir} regreet -l debug; swaymsg exit"
+    '';
+  in
+    pkgs.writeShellApplication {
+      name = "greeter";
+      runtimeInputs = [runSway runRiver pkgs.nushell pkgs.systemd pkgs.greetd.regreet pkgs.sway pkgs.swaybg];
+      text = ''
+        echo "Starting sway-greeter" | tee -a /tmp/sway-greeter.log
+        export XDG_DATA_DIRS=${sessionDir}
+        export XDG_SESSION_TYPE="wayland"
+        export XDG_CURRENT_DESKTOP="sway"
+        export XDG_SESSION_DESKTOP="sway"
+        ${pkgs.dbus}/bin/dbus-update-activation-environment --systemd --all
+        ${pkgs.dbus}/bin/dbus-run-session ${lib.getExe pkgs.sway} -c ${swayConfig} 2>&1 | tee -a /tmp/sway-greeter.log
+      '';
+    };
 in {
+  environment.systemPackages = [
+    pkgs.nordzy-cursor-theme
+    pkgs.nordic
+    pkgs.arc-icon-theme
+    pkgs.roboto
+  ];
+  programs.regreet = {
+    enable = true;
+    settings = {
+      background = {
+        path = regreetBg;
+        fit = "Cover";
+      };
+      gtk = {
+        application_prefer_dark_theme = false;
+        cursor_theme = "Nordzy-cursors";
+        icon_theme_name = "Arc";
+        theme_name = "Nordic-darker";
+        font_name = "Roboto Medium 11";
+      };
+      commands = {
+        reboot = ["systemctl" "reboot"];
+        poweroff = ["systemctl" "poweroff"];
+      };
+    };
+  };
   services.greetd = {
     enable = true;
     restart = true;
     settings = {
-      default_session.command = "${createGreeter "sway" sessions}/bin/greeter";
+      #default_session.command = "${createTuiGreeter "sway" sessions}/bin/greeter";
+      default_session.command = "${createRegreetGreeter sessions}/bin/greeter";
     };
   };
   ## prevents systemd spewing the console with log messages when greeter is active
