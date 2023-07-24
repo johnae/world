0ac11537843db84204f7729b020cab15cdfc91c8
John Axel Eriksson <john@insane.se>
2023-07-25 00:11:15 +0200

    foot: tweak font size, add foot config on eris/antares

--- users/profiles/foot.nix
+++ users/profiles/foot.nix
@@ -4,7 +4,7 @@ _: {
     server.enable = true;
     settings = {
       main = {
-        font = "JetBrainsMono Nerd Font:size=8";
+        font = "JetBrainsMono Nerd Font:size=14";
         term = "foot";
         dpi-aware = "yes";
       };
