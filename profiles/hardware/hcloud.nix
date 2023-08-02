20c53681302cbf4504e589bfe5bfa5e5340a9576
John Axel Eriksson <john@insane.se>
2023-08-03 00:46:14 +0200

    Improve hcloud profile

--- profiles/hardware/hcloud.nix
+++ profiles/hardware/hcloud.nix
@@ -1,6 +1,15 @@
-{modulesPath, ...}: {
+{
+  modulesPath,
+  hostName,
+  pkgs,
+  inputs,
+  config,
+  lib,
+  ...
+}: {
   imports = [
     (modulesPath + "/profiles/qemu-guest.nix")
+    ../../cachix.nix
   ];
   boot.loader.grub.device = "/dev/sda";
   boot.initrd.availableKernelModules = ["ata_piix" "uhci_hcd" "xen_blkfront" "vmw_pvscsi"];
@@ -9,8 +18,58 @@
     device = "/dev/sda1";
     fsType = "ext4";
   };
+  boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.tmp.cleanOnBoot = true;
   zramSwap.enable = true;
-  networking.hostName = "hcloud-nixos";
+  networking.hostName = hostName;
   services.openssh.enable = true;
+
+  nix = {
+    settings.trusted-users = ["root"];
+    extraOptions = ''
+      experimental-features = nix-command flakes
+      keep-outputs = true
+      keep-derivations = true
+      tarball-ttl = 900
+    '';
+
+    registry.nixpkgs.flake = inputs.nixpkgs;
+
+    nixPath = ["nixpkgs=${inputs.nixpkgs}"];
+
+    gc = {
+      automatic = true;
+      dates = "daily";
+      options = "--delete-older-than 7d";
+    };
+
+    package = pkgs.nix;
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = [
+    pkgs.binutils
+    pkgs.cacert
+    pkgs.curl
+    pkgs.fd
+    pkgs.file
+    pkgs.git
+    pkgs.iptables
+    pkgs.jq
+    pkgs.lsof
+    pkgs.man-pages
+    pkgs.mkpasswd
+    pkgs.nmap
+    pkgs.openssl
+    pkgs.procs
+    pkgs.psmisc
+    pkgs.ripgrep
+    pkgs.sd
+    pkgs.tree
+    pkgs.unzip
+    pkgs.vim
+    pkgs.wget
+    pkgs.zip
+  ];
 }
