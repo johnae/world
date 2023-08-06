0708c4e60956b670004f59cf27b40edfa830233a
John Axel Eriksson <john@insane.se>
2023-08-06 23:44:48 +0200

    hcloud: remove cloud-init

--- profiles/hardware/hcloud.nix
+++ profiles/hardware/hcloud.nix
@@ -26,14 +26,6 @@
   networking.hostName = hostName;
   services.openssh.enable = true;
 
-  services.cloud-init = {
-    enable = true;
-    xfs.enable = false;
-    ext4.enable = false;
-    btrfs.enable = false;
-    network.enable = true;
-  };
-
   nix = {
     settings.trusted-users = ["root"];
     extraOptions = ''
