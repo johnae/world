519a8705a3f7c5daa003a06e8965d6b56bf057cd
John Axel Eriksson <john@insane.se>
2023-08-15 15:55:40 +0200

    hcloud: add cryptsetup, add additional filesystems

--- profiles/hcloud-k3s-core.nix
+++ profiles/hcloud-k3s-core.nix
@@ -11,5 +11,29 @@
     pkgs.k3s
     pkgs.kubectl
     pkgs.tailscale
+    pkgs.cryptsetup
   ];
+  fileSystems."/data" = {
+    encrypted = {
+      label = "cryptdata";
+      keyFile = "/sys/class/dmi/id/product_uuid";
+      enable = true;
+      blkDev = "/dev/sdb";
+    };
+    device = "/dev/mapper/data";
+    fsType = "ext4";
+    autoFormat = true;
+  };
+  fileSystems."/var/lib/rancher" = {
+    device = "/data/rancher";
+    fsType = "none";
+    autoFormat = false;
+    options = ["bind"];
+  };
+  fileSystems."/var/lib/kubelet" = {
+    device = "/data/kubelet";
+    fsType = "none";
+    autoFormat = false;
+    options = ["bind"];
+  };
 }
