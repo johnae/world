652d1df111a4f91dc6c2f0094bbb06274c38a6f6
John Axel Eriksson <john@insane.se>
2023-09-12 23:53:37 +0200

    Tweak vaultwarden pkg

---
+++ profiles/vaultwarden.nix
@@ -0,0 +1,34 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}: let
+  cfg = config.services.vaultwarden;
+  user = config.users.users.vaultwarden.name;
+  group = config.users.users.vaultwarden.group;
+  backup = pkgs.writeText "backup.sh" ''
+    mkdir -p "$DIR";
+    ${pkgs.sqlite}/bin/sqlite3 /var/lib/bitwarden_rs/db.sqlite3 "VACUUM INTO '${cfg.backupDir}/db.sqlite3'";
+    cp -R /var/lib/bitwarden_rs/{attachments,sends,rsa_key*,icon_cache} "${cfg.backupDir}"/;
+  '';
+in {
+  systemd.services.backup-vaultwarden = lib.mkForce {
+    description = "Backup vaultwarden";
+    environment = {
+      DATA_FOLDER = "/var/lib/bitwarden_rs";
+      BACKUP_FOLDER = cfg.backupDir;
+    };
+    path = with pkgs; [sqlite];
+    # if both services are started at the same time, vaultwarden fails with "database is locked"
+    before = ["vaultwarden.service"];
+    serviceConfig = {
+      SyslogIdentifier = "backup-vaultwarden";
+      Type = "oneshot";
+      User = lib.mkDefault user;
+      Group = lib.mkDefault group;
+      ExecStart = "${pkgs.bash}/bin/bash ${backup}";
+    };
+    wantedBy = ["multi-user.target"];
+  };
+}
