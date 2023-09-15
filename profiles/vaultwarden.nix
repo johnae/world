04e26ad64f28d65909d2d649ad3493494042e60c
John Axel Eriksson <john@insane.se>
2023-09-15 12:35:49 +0200

    Just do what the linter says :-|

--- profiles/vaultwarden.nix
+++ profiles/vaultwarden.nix
@@ -6,7 +6,7 @@
 }: let
   cfg = config.services.vaultwarden;
   user = config.users.users.vaultwarden.name;
-  group = config.users.users.vaultwarden.group;
+  inherit (config.users.users.vaultwarden) group;
   backup = pkgs.writeText "backup.sh" ''
     ${pkgs.sqlite}/bin/sqlite3 /var/lib/bitwarden_rs/db.sqlite3 "VACUUM INTO '${cfg.backupDir}/db.sqlite3'";
     cp -R /var/lib/bitwarden_rs/{attachments,sends,rsa_key*,icon_cache} "${cfg.backupDir}"/;
