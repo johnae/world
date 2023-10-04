19e3c621681bf608f846f93e73806626d7f8e027
John Axel Eriksson <john@insane.se>
2023-10-04 20:02:18 +0200

    tweak helix config

--- users/profiles/helix.nix
+++ users/profiles/helix.nix
@@ -6,7 +6,10 @@
 
       editor = {
         line-number = "relative";
-        mouse = false;
+        mouse = true;
+        cursorline = true;
+        bufferline = "multiple";
+        true-color = true;
 
         cursor-shape = {
           insert = "bar";
@@ -17,6 +20,13 @@
         file-picker = {
           hidden = false;
         };
+
+        lsp = {
+          auto-signature-help = false;
+          display-messages = true;
+        };
+
+        statusline.left = ["mode" "spinner" "version-control" "file-name"];
       };
     };
     languages = {
@@ -24,7 +34,7 @@
         {
           name = "nix";
           formatter = {command = "${pkgs.alejandra}/bin/alejandra";};
-          language-server = {command = "${pkgs.nixd}/bin/nixd";};
+          language-server = {command = "${pkgs.nil}/bin/nil";};
           auto-format = true;
         }
       ];
