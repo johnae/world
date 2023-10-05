50969a499e1876c0ad6fc3c2f73519cf5060013c
John Axel Eriksson <john@insane.se>
2023-10-05 15:02:58 +0200

    wezterm: fix color scheme

--- users/profiles/wezterm.nix
+++ users/profiles/wezterm.nix
@@ -6,7 +6,7 @@
       local config = wezterm.config_builder()
       config.font = wezterm.font 'JetBrainsMono Nerd Font'
       config.font_size = 14.0
-      config.color_scheme = 'Nord'
+      config.color_scheme = 'nord'
       config.hide_tab_bar_if_only_one_tab = true
       return config
     '';
