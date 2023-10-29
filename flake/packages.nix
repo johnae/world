d793953fba379a3f2ead2304e74e2bdc9bd5dcba
John Axel Eriksson <john@insane.se>
2023-10-29 14:54:30 +0100

    helix: patch upstream helix instead of pointing to fork

--- flake/packages.nix
+++ flake/packages.nix
@@ -24,15 +24,66 @@
       name: _: (pkgs.callPackage (../packages + "/${name}") {inherit inputs;})
     ) (filterAttrs (filename: type: type == "directory") (readDir ../packages));
 
+    mkRootPath = rel:
+      builtins.path {
+        path = "${inputs.helix}/${rel}";
+        name = rel;
+      };
+
+    makeOverridableHelix = old: config: let
+      grammars = pkgs.callPackage "${inputs.helix}/grammars.nix" config;
+      runtimeDir = pkgs.runCommand "helix-runtime" {} ''
+        mkdir -p $out
+        ln -s ${mkRootPath "runtime"}/* $out
+        rm -r $out/grammars
+        ln -s ${grammars} $out/grammars
+      '';
+      helix-wrapped =
+        pkgs.runCommand
+        old.name
+        {
+          inherit (old) pname version;
+          meta = old.meta or {};
+          passthru =
+            (old.passthru or {})
+            // {
+              unwrapped = old;
+            };
+          nativeBuildInputs = [pkgs.makeWrapper];
+          makeWrapperArgs = config.makeWrapperArgs or [];
+        }
+        ''
+          cp -rs --no-preserve=mode,ownership ${old} $out
+          wrapProgram "$out/bin/hx" ''${makeWrapperArgs[@]} --set HELIX_RUNTIME "${runtimeDir}"
+        '';
+    in
+      helix-wrapped
+      // {
+        override = makeOverridableHelix old;
+        passthru =
+          helix-wrapped.passthru
+          // {
+            wrapper = old: makeOverridableHelix old config;
+          };
+      };
+
     packages =
       locallyDefinedPackages
-      // rec {
+      // {
         inherit (pkgs.callPackage ../utils/world.nix {}) pixieboot world lint;
         conduit = inputs.matrix-conduit.packages.${system}.default;
       }
-      // {
+      // rec {
         ## packages from other flakes
-        helix-latest = inputs.helix.packages.${system}.helix;
+        helix-unwrapped-latest = inputs.helix.packages.${system}.helix-unwrapped.overrideAttrs (oa: {
+          src = pkgs.runCommand "patched-source" {} ''
+            cp -r ${oa.src} $out
+            chmod -R u+w $out
+            cd $out
+            patch -f -p1 -t < ${inputs.helix-copilot-patch} || true
+          '';
+        });
+        helix-latest = makeOverridableHelix helix-unwrapped-latest {};
         hyprland-unstable = inputs.hyprland.packages.${system}.hyprland;
         inherit
           (inputs.hyprland.packages.${system})
