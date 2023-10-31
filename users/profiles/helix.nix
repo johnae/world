66da4b3840452e59048b593b93a0820db3abda0a
John Axel Eriksson <john@insane.se>
2023-10-31 12:12:27 +0100

    helix: rust-analyzer, run clippy on save

--- users/profiles/helix.nix
+++ users/profiles/helix.nix
@@ -78,6 +78,9 @@ in {
           command = "${copilot}/bin/copilot";
           args = ["--stdio"];
         };
+        rust-analyzer = {
+          config.check.command = "clippy";
+        };
         yaml-language-server = {
           config.yaml.format.enable = true;
           config.yaml.validation = true;
