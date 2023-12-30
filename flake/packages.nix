b705fa4dca48bd2d24144ff0c8042250e3a33de0
John Axel Eriksson <john@insane.se>
2023-12-31 00:29:01 +0100

    Add diskformat package (removed earlier)

--- flake/packages.nix
+++ flake/packages.nix
@@ -1,17 +1,31 @@
-{inputs, ...}: {
+{
+  inputs,
+  self,
+  ...
+}: {
   perSystem = {
     pkgs,
     lib,
     system,
     ...
   }: let
-    inherit (lib // builtins) filterAttrs mapAttrs readDir;
+    inherit (lib // builtins) filterAttrs mapAttrs readDir mapAttrs';
     locallyDefinedPackages = mapAttrs (
       name: _: (pkgs.callPackage (../packages + "/${name}") {inherit inputs;})
     ) (filterAttrs (filename: type: type == "directory") (readDir ../packages));
   in {
     packages =
-      locallyDefinedPackages
+      (
+        mapAttrs' (hostname: config: {
+          name = "${hostname}-diskformat";
+          value = pkgs.callPackage ../utils/diskformat.nix {
+            inherit config;
+            inherit lib;
+          };
+        })
+        self.nixosConfigurations
+      )
+      // locallyDefinedPackages
       // {
         world = pkgs.writeShellApplication {
           name = "world";
