ae25ba3cf9ce82681dc9977b3d8957dcfccfdc40
John Axel Eriksson <john@insane.se>
2024-01-02 08:21:51 +0100

    river/waybar: use tag icons

--- users/profiles/waybar.nix
+++ users/profiles/waybar.nix
@@ -101,7 +101,7 @@ in {
     };
     "river/tags" = {
       num-tags = 9;
-      tag-labels = ["1: dev" "2: chromium-work" "3: firefox" "4: chromium-private" "5" "6" "7" "8" "9: chat"];
+      tag-labels = ["1: " "2:  work" "3: " "4:  private" "5" "6" "7" "8" "9: "];
       set-tags = [
         1
         2
