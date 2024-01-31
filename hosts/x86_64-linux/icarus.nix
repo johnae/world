9ad3e7aa997d4d99373d8fb111c78c9fe94fb393
John Axel Eriksson <john@insane.se>
2024-01-31 14:44:00 +0100

    icarus: updates

--- hosts/x86_64-linux/icarus.nix
+++ hosts/x86_64-linux/icarus.nix
@@ -3,18 +3,22 @@
   hostName,
   ...
 }: {
-  publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMfXrbXEQcJ85CGW9/qnm/jS8Vp6GP2yQ2piFF8+OFWR";
+  publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHaa82NwBC+ty4Wyeuf5kdava7huSYF6k0NYF2ahwayW";
   syncthingDeviceID = "HBL5ZRB-R2STGW5-LMAYYHX-KOFTP3X-VO4IV6E-PEDKZ3N-WCRR7BY-F5C7AAP";
 
-  btrfs.disks = ["/dev/nvme0n1" "/dev/nvme1n1"];
+  bcachefs = {
+    disks = ["/dev/nvme0n1" "/dev/nvme1n1"];
+    devices = ["/dev/nvme0n1p3" "/dev/nvme1n1p1"];
+  };
 
   imports = [
     ../../profiles/admin-user/home-manager.nix
     ../../profiles/admin-user/user.nix
-    ../../profiles/disk/btrfs-on-luks.nix
+    ../../profiles/disk/bcachefs.nix
     ../../profiles/hardware/b550.nix
     ../../profiles/home-manager.nix
     ../../profiles/server.nix
+    ../../profiles/restic-backup.nix
     ../../profiles/state.nix
     ../../profiles/syncthing.nix
     ../../profiles/tailscale.nix
@@ -33,6 +37,51 @@
 
   ## for tailscale exit node functionality
   boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
+
+  boot.kernelParams = [
+    "ip=192.168.20.143::192.168.20.1:255.255.255.0:${hostName}:eth0:none"
+  ];
+
+  boot.initrd.systemd = {
+    enable = true;
+    emergencyAccess = true;
+    network = {
+      enable = true;
+      networks."eth0".extraConfig = ''
+        [Match]
+        Name = eth0
+        [Network]
+        Address = 2a01:4f9:3051:5389::2/64
+        Gateway = fe80:1
+        Address = 65.109.85.161/26
+        Gateway = 65.109.85.129
+      '';
+    };
+  };
+
+  # boot.initrd.network = {
+  #   enable = true;
+  #   postCommands = "echo 'systemctl default' >> /root/.profile";
+  # };
+
+  boot.initrd.network = {
+    enable = true;
+    flushBeforeStage2 = true;
+    ssh = {
+      enable = true;
+      port = 2222;
+      ## This isn't so nice. Have to copy the file to /keep/secrets and keep it there.
+      hostKeys = [
+        "/keep/secrets/initrd_ed25519_key"
+      ];
+      authorizedKeys = [
+        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCyjMuNOFrZBi7CrTyu71X+aRKyzvTmwCEkomhB0dEhENiQ3PTGVVWBi1Ta9E9fqbqTW0HmNL5pjGV+BU8j9mSi6VxLzJVUweuwQuvqgAi0chAJVPe0FSzft9M7mJoEq5DajuSiL7dSjXpqNFDk/WCDUBE9pELw+TXvxyQpFO9KZwiYCCNRQY6dCjrPJxGwG+JzX6l900GFrgOXQ3KYGk8vzep2Qp+iuH1yTgEowUICkb/9CmZhHQXSvq2gAtoOsGTd9DTyLOeVwZFJkTL/QW0AJNRszckGtYdA3ftCUNsTLSP/VqYN9EjxcMHQe4PGjkK7VLb59DQJFyRQqvPXiUyxNloHcu/sDuiKHIk/0qDLHlVn2xc5zkvzSqoQxoXx+P4dDbje1KHLY8E96gLe2Csu0ti+qsM5KEvgYgwWwm2g3IBlaWwgAtC0UWEzIuBPrAgPd5vi+V50ITIaIk6KIV7JPOubLUXaLS5KW77pWyi9PqAGOXj+DgTWoB3QeeZh7CGhPL5fAecYN7Pw734cULZpnw10Bi/jp4Nlq1AJDk8BwLUJbzZ8aexwMf78syjkHJBBrTOAxADUE02nWBQd0w4K5tl/a3UnBYWGyX8TD44046Swl/RY/69PxFvYcVRuF4eARI6OWojs1uhoR9WkO8eGgEsuxxECwNpWxR5gjKcgJQ=="
+        "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIJY3QSBIiRKN8/B3nHgCBDp;auQBOftphOeuF2TaBHGQSAAAABHNzaDo="
+        "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIAwJWtQ5ZU9U0szWzJ+/GH2uvXZ15u9lL0RdcHdsXM0VAAAABHNzaDo="
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK+trOinD68RD1efI6p05HeaNA0SjzeRnUvpf22+jsq+"
+      ];
+    };
+  };
 
   services.tailscale.auth = {
     enable = true;
