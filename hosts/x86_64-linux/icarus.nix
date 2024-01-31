e1150f4c4624ca86d490059cc7fb005b4d6432c3
John Axel Eriksson <john@insane.se>
2024-01-31 22:03:00 +0100

    icarus/bcachefs: try script stage1 again

--- hosts/x86_64-linux/icarus.nix
+++ hosts/x86_64-linux/icarus.nix
@@ -6,9 +6,9 @@
   publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHaa82NwBC+ty4Wyeuf5kdava7huSYF6k0NYF2ahwayW";
   syncthingDeviceID = "HBL5ZRB-R2STGW5-LMAYYHX-KOFTP3X-VO4IV6E-PEDKZ3N-WCRR7BY-F5C7AAP";
 
-  bcachefs = rec {
+  bcachefs = {
     disks = ["/dev/nvme0n1" "/dev/nvme1n1"];
-    devices = ["/dev/disk/by-uuid/8bb1d35f-475b-4c38-9603-25b1a0bad57f"];
+    devices = ["/dev/disk/by-partlabel/p_root" "/dev/disk/by-partlabel/p_root1"];
   };
 
   imports = [
@@ -41,28 +41,24 @@
     "ip=192.168.20.143::192.168.20.1:255.255.255.0:${hostName}:eth0:none"
   ];
 
-  boot.initrd.systemd = {
-    enable = true;
-    emergencyAccess = true;
-    network = {
-      enable = true;
-      networks."eth0".extraConfig = ''
-        [Match]
-        Name = eth0
-        [Network]
-        Address = 192.168.20.143/24
-        Gateway = 192.168.20.1
-      '';
-    };
-  };
-
-  # boot.initrd.network = {
+  # boot.initrd.systemd = {
   #   enable = true;
-  #   postCommands = "echo 'systemctl default' >> /root/.profile";
+  #   emergencyAccess = true;
+  #   network = {
+  #     enable = true;
+  #     networks."eth0".extraConfig = ''
+  #       [Match]
+  #       Name = eth0
+  #       [Network]
+  #       Address = 192.168.20.143/24
+  #       Gateway = 192.168.20.1
+  #     '';
+  #   };
   # };
 
   boot.initrd.network = {
     enable = true;
+    postCommands = "echo 'bcachefs unlock /dev/disk/by-partlabel/p_root:/dev/disk/by-partlabel/p_root1' >> /root/.profile";
     flushBeforeStage2 = true;
     ssh = {
       enable = true;
@@ -79,6 +75,25 @@
       ];
     };
   };
+
+  # boot.initrd.network = {
+  #   enable = true;
+  #   flushBeforeStage2 = true;
+  #   ssh = {
+  #     enable = true;
+  #     port = 2222;
+  #     ## This isn't so nice. Have to copy the file to /keep/secrets and keep it there.
+  #     hostKeys = [
+  #       "/keep/secrets/initrd_ed25519_key"
+  #     ];
+  #     authorizedKeys = [
+  #       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCyjMuNOFrZBi7CrTyu71X+aRKyzvTmwCEkomhB0dEhENiQ3PTGVVWBi1Ta9E9fqbqTW0HmNL5pjGV+BU8j9mSi6VxLzJVUweuwQuvqgAi0chAJVPe0FSzft9M7mJoEq5DajuSiL7dSjXpqNFDk/WCDUBE9pELw+TXvxyQpFO9KZwiYCCNRQY6dCjrPJxGwG+JzX6l900GFrgOXQ3KYGk8vzep2Qp+iuH1yTgEowUICkb/9CmZhHQXSvq2gAtoOsGTd9DTyLOeVwZFJkTL/QW0AJNRszckGtYdA3ftCUNsTLSP/VqYN9EjxcMHQe4PGjkK7VLb59DQJFyRQqvPXiUyxNloHcu/sDuiKHIk/0qDLHlVn2xc5zkvzSqoQxoXx+P4dDbje1KHLY8E96gLe2Csu0ti+qsM5KEvgYgwWwm2g3IBlaWwgAtC0UWEzIuBPrAgPd5vi+V50ITIaIk6KIV7JPOubLUXaLS5KW77pWyi9PqAGOXj+DgTWoB3QeeZh7CGhPL5fAecYN7Pw734cULZpnw10Bi/jp4Nlq1AJDk8BwLUJbzZ8aexwMf78syjkHJBBrTOAxADUE02nWBQd0w4K5tl/a3UnBYWGyX8TD44046Swl/RY/69PxFvYcVRuF4eARI6OWojs1uhoR9WkO8eGgEsuxxECwNpWxR5gjKcgJQ=="
+  #       "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIJY3QSBIiRKN8/B3nHgCBDp;auQBOftphOeuF2TaBHGQSAAAABHNzaDo="
+  #       "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIAwJWtQ5ZU9U0szWzJ+/GH2uvXZ15u9lL0RdcHdsXM0VAAAABHNzaDo="
+  #       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK+trOinD68RD1efI6p05HeaNA0SjzeRnUvpf22+jsq+"
+  #     ];
+  #   };
+  # };
 
   services.tailscale.auth = {
     enable = true;
