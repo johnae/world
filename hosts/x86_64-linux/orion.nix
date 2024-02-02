08d8516c08424a6a7508c1dc62c20132276b2603
John Axel Eriksson <john@insane.se>
2024-02-02 11:01:58 +0100

    orion: also add vaultwarden profile

--- hosts/x86_64-linux/orion.nix
+++ hosts/x86_64-linux/orion.nix
@@ -22,7 +22,7 @@
     ../../profiles/state.nix
     ../../profiles/syncthing.nix
     ../../profiles/tailscale.nix
-    #    ../../profiles/vaultwarden.nix
+    ../../profiles/vaultwarden.nix
     ../../profiles/zram.nix
   ];
 
