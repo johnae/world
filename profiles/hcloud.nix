df5304ad886d2ef59ba21fabd1629b888d7d87ed
John Axel Eriksson <john@insane.se>
2024-04-05 16:26:09 +0200

    hcloud: tweak bash prompt

--- profiles/hcloud.nix
+++ profiles/hcloud.nix
@@ -42,6 +42,25 @@ in {
     };
   };
 
+  environment.etc."bashrc.local".text = ''
+    if [ -e /etc/generated-hostname ]; then
+      NODENAME="$(cat /etc/generated-hostname)"
+      if [ "$TERM" != "dumb" ] || [ -n "$INSIDE_EMACS" ]; then
+        PROMPT_COLOR="1;31m"
+        ((UID)) && PROMPT_COLOR="1;32m"
+        if [ -n "$INSIDE_EMACS" ]; then
+          # Emacs term mode doesn't support xterm title escape sequence (\e]0;)
+          PS1="\n\[\033[$PROMPT_COLOR\][\u@\h|$NODENAME:\w]\\$\[\033[0m\] "
+        else
+          PS1="\n\[\033[$PROMPT_COLOR\][\[\e]0;\u@\h|$NODENAME: \w\a\]\u@\h|$NODENAME:\w]\\$\[\033[0m\] "
+        fi
+        if test "$TERM" = "xterm"; then
+          PS1="\[\033]2;\h|$NODENAME:\u:\w\007\]$PS1"
+        fi
+      fi
+    fi
+  '';
+
   networking.usePredictableInterfaceNames = false;
   networking.dhcpcd.enable = false;
   networking.useDHCP = false;
