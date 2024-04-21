e11ed82e9ed4f6263b7d0ee0dbcc578ebca1f83a
John Axel Eriksson <john@insane.se>
2024-04-21 17:10:03 +0200

    Add zjstatus zellij statusbar + flake fixes

--- users/profiles/zellij.nix
+++ users/profiles/zellij.nix
@@ -50,7 +50,34 @@ in {
 
 
         pane size=1 borderless=true {
-          plugin location="zellij:compact-bar"
+          plugin location="file:${pkgs.zjstatus}/bin/zjstatus.wasm" {
+                format_left   "{mode} #[fg=#89B4FA,bold]{session}"
+                format_center "{tabs}"
+                format_right  "{command_git_branch} {datetime}"
+                format_space  ""
+
+                border_enabled  "false"
+                border_char     "─"
+                border_format   "#[fg=#6C7086]{char}"
+                border_position "top"
+
+                hide_frame_for_single_pane "true"
+
+                mode_normal  "#[bg=blue] "
+                mode_tmux    "#[bg=#ffc387] "
+
+                tab_normal   "#[fg=#6C7086] {name} "
+                tab_active   "#[fg=#9399B2,bold,italic] {name} "
+
+                command_git_branch_command     "git rev-parse --abbrev-ref HEAD"
+                command_git_branch_format      "#[fg=blue] {stdout} "
+                command_git_branch_interval    "10"
+                command_git_branch_rendermode  "static"
+
+                datetime        "#[fg=#6C7086,bold] {format} "
+                datetime_format "%A, %d %b %Y %H:%M"
+                datetime_timezone "Europe/Stockholm"
+            }
         }
 
         floating_panes {
@@ -70,7 +97,34 @@ in {
         pane
       }
       pane size=1 borderless=true {
-        plugin location="zellij:compact-bar"
+        plugin location="file:${pkgs.zjstatus}/bin/zjstatus.wasm" {
+              format_left   "{mode} #[fg=#89B4FA,bold]{session}"
+              format_center "{tabs}"
+              format_right  "{command_git_branch} {datetime}"
+              format_space  ""
+
+              border_enabled  "false"
+              border_char     "─"
+              border_format   "#[fg=#6C7086]{char}"
+              border_position "top"
+
+              hide_frame_for_single_pane "true"
+
+              mode_normal  "#[bg=blue] "
+              mode_tmux    "#[bg=#ffc387] "
+
+              tab_normal   "#[fg=#6C7086] {name} "
+              tab_active   "#[fg=#9399B2,bold,italic] {name} "
+
+              command_git_branch_command     "git rev-parse --abbrev-ref HEAD"
+              command_git_branch_format      "#[fg=blue] {stdout} "
+              command_git_branch_interval    "10"
+              command_git_branch_rendermode  "static"
+
+              datetime        "#[fg=#6C7086,bold] {format} "
+              datetime_format "%A, %d %b %Y %H:%M"
+              datetime_timezone "Europe/Stockholm"
+          }
       }
     }
   '';
