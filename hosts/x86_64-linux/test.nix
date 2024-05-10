edd3ac0fde7813ae4c3baa62c07fda8cad55317c
John Axel Eriksson <john@insane.se>
2024-05-10 10:17:09 +0200

    Test vm tweaks

--- hosts/x86_64-linux/test.nix
+++ hosts/x86_64-linux/test.nix
@@ -32,11 +32,13 @@
 
   boot.kernelParams = [
     "ip=dhcp"
+    "console=ttyS0"
   ];
 
   boot.initrd.availableKernelModules = [
     "igb"
     "nvme"
+    "e1000e"
     "ahci"
     "usbhid"
     "virtio"
@@ -47,29 +49,6 @@
   # };
   # lsblk -f
 
-  boot.initrd.systemd = let
-    mount = pkgs.writeShellScriptBin "bcachefs-mount-helper" ''
-      mkdir /sysroot
-      until bcachefs mount -o rw,relatime,metadata_replicas=2,compression=zstd,background_compression=zstd /dev/mapper/encrypted_root:/dev/mapper/encrypted_root1 /sysroot; do
-        sleep 1
-      done
-    '';
-  in {
-    enable = true;
-    storePaths = ["${mount}/bin/bcachefs-mount-helper"];
-    users.root.shell = "${mount}/bin/bcachefs-mount-helper";
-    # services.bcachefs-mount-helper = {
-    #   description = "Mount the bcachefs root filesystem";
-    #   before = ["sysroot-keep.mount"];
-    #   # after = ["local-fs.target"];
-    #   # wants = ["local-fs.target"];
-    #   serviceConfig = {
-    #     Type = "oneshot";
-    #     ExecStart = "${mount}/bin/bcachefs-mount-helper";
-    #   };
-    # };
-  };
-
   boot.initrd.network = {
     enable = true;
     # postCommands = "echo 'cryptsetup-askpass' >> /root/.profile";
@@ -79,13 +58,11 @@
       port = 2222;
       ## This isn't so nice. Have to copy the file to /keep/secrets and keep it there.
       hostKeys = [
-        /keep/secrets/initrd_ed25519_key
+        "/keep/secrets/initrd_ed25519_key"
       ];
       authorizedKeys = [
-        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCyjMuNOFrZBi7CrTyu71X+aRKyzvTmwCEkomhB0dEhENiQ3PTGVVWBi1Ta9E9fqbqTW0HmNL5pjGV+BU8j9mSi6VxLzJVUweuwQuvqgAi0chAJVPe0FSzft9M7mJoEq5DajuSiL7dSjXpqNFDk/WCDUBE9pELw+TXvxyQpFO9KZwiYCCNRQY6dCjrPJxGwG+JzX6l900GFrgOXQ3KYGk8vzep2Qp+iuH1yTgEowUICkb/9CmZhHQXSvq2gAtoOsGTd9DTyLOeVwZFJkTL/QW0AJNRszckGtYdA3ftCUNsTLSP/VqYN9EjxcMHQe4PGjkK7VLb59DQJFyRQqvPXiUyxNloHcu/sDuiKHIk/0qDLHlVn2xc5zkvzSqoQxoXx+P4dDbje1KHLY8E96gLe2Csu0ti+qsM5KEvgYgwWwm2g3IBlaWwgAtC0UWEzIuBPrAgPd5vi+V50ITIaIk6KIV7JPOubLUXaLS5KW77pWyi9PqAGOXj+DgTWoB3QeeZh7CGhPL5fAecYN7Pw734cULZpnw10Bi/jp4Nlq1AJDk8BwLUJbzZ8aexwMf78syjkHJBBrTOAxADUE02nWBQd0w4K5tl/a3UnBYWGyX8TD44046Swl/RY/69PxFvYcVRuF4eARI6OWojs1uhoR9WkO8eGgEsuxxECwNpWxR5gjKcgJQ=="
-        "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIJY3QSBIiRKN8/B3nHgCBDp;auQBOftphOeuF2TaBHGQSAAAABHNzaDo="
-        "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIAwJWtQ5ZU9U0szWzJ+/GH2uvXZ15u9lL0RdcHdsXM0VAAAABHNzaDo="
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK+trOinD68RD1efI6p05HeaNA0SjzeRnUvpf22+jsq+"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIzm5RyD+1nfy1LquvkEog4SZtPgdhzjr49jSC8PAinp"
       ];
     };
   };
