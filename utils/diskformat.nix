3a1af2a42f1288a7859414fa911131188a2d96c1
John Axel Eriksson <john@insane.se>
2024-05-10 16:35:29 +0200

    bcachefs-on-luks: stop bind mounting home/var etc

--- utils/diskformat.nix
+++ utils/diskformat.nix
@@ -6,25 +6,7 @@
   ...
 }: let
   bcachefsDisks = config.config.bcachefs.disks;
-  adminUser = config.config.adminUser;
-  adminUserFiles = config.config.environment.persistence."/keep".users.${adminUser.name}.files;
-  userFiles =
-    if adminUser != {}
-    then ''
-      ${lib.concatStringsSep "\n" (map (f: "mkdir -p /mnt/keep${f.parentDirectory.dirPath}; touch /mnt/keep${f.filePath}") adminUserFiles)}
-      chown -R ${toString adminUser.uid}:users /mnt/keep/home/${adminUser.name}
-    ''
-    else "";
 in
-  writeShellApplication {
-    name = "diskformat";
-    runtimeInputs = [keyutils];
-    text = ''
-      ${
-        if builtins.length bcachefsDisks > 0
-        then import ./diskformat-bcachefs-dmcrypt.nix {inherit config lib keyutils writeShellApplication;}
-        else import ./diskformat-btrfs.nix {inherit config lib writeShellApplication;}
-      }/bin/diskformat
-      ${userFiles}
-    '';
-  }
+  if builtins.length bcachefsDisks > 0
+  then import ./diskformat-bcachefs-dmcrypt.nix {inherit config lib keyutils writeShellApplication;}
+  else import ./diskformat-btrfs.nix {inherit config lib writeShellApplication;}
