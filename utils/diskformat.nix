4252399d66b30dcf1c39f93e107a8527d5fdb01b
John Axel Eriksson <john@insane.se>
2024-05-10 15:57:36 +0200

    Fix non-existant files linking - impermanence

--- utils/diskformat.nix
+++ utils/diskformat.nix
@@ -3,29 +3,28 @@
   lib,
   writeShellApplication,
   keyutils,
-  adminUser,
   ...
 }: let
   bcachefsDisks = config.config.bcachefs.disks;
+  adminUser = config.config.adminUser;
+  adminUserFiles = config.config.environment.persistence."/keep".users.${adminUser.name}.files;
   userFiles =
-    if adminUser != null
+    if adminUser != {}
     then ''
-      mkdir -p /mnt/keep/home/${adminUser.name}/.config/nushell
-      mkdir -p /mnt/keep/home/${adminUser.name}/.kube
-      mkdir -p /mnt/keep/home/${adminUser.name}/.ssh
-      touch /mnt/keep/home/${adminUser.name}/.config/nushell/history.txt
-      touch /mnt/keep/home/${adminUser.name}/.config/tenacity.cfg
-      touch /mnt/keep/home/${adminUser.name}/.kube/config
-      touch /mnt/keep/home/${adminUser.name}/.spotify_token_cache.json
-      touch /mnt/keep/home/${adminUser.name}/.ssh/known_hosts
-      chown -R ${adminUser.uid}:users /mnt/keep/home/${adminUser.name}
+      ${lib.concatStringsSep "\n" (map (f: "mkdir -p /mnt/keep${f.parentDirectory.dirPath}; touch /mnt/keep${f.filePath}") adminUserFiles)}
+      chown -R ${toString adminUser.uid}:users /mnt/keep/home/${adminUser.name}
     ''
     else "";
-in ''
-  ${
-    if builtins.length bcachefsDisks > 0
-    then import ./diskformat-bcachefs-dmcrypt.nix {inherit config lib keyutils writeShellApplication;}
-    else import ./diskformat-btrfs.nix {inherit config lib writeShellApplication;}
+in
+  writeShellApplication {
+    name = "diskformat";
+    runtimeInputs = [keyutils];
+    text = ''
+      ${
+        if builtins.length bcachefsDisks > 0
+        then import ./diskformat-bcachefs-dmcrypt.nix {inherit config lib keyutils writeShellApplication;}
+        else import ./diskformat-btrfs.nix {inherit config lib writeShellApplication;}
+      }/bin/diskformat
+      ${userFiles}
+    '';
   }
-  ${userFiles}
-''
