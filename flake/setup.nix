42c632ee0a31c0932826b7b57b22f75cc1c6679b
John Axel Eriksson <john@insane.se>
2024-05-17 12:16:41 +0200

    google-cloud-sdk: stop patching config-connector

--- flake/setup.nix
+++ flake/setup.nix
@@ -43,23 +43,10 @@
 
         (final: prev: {
           inherit inputs;
-          google-cloud-sdk-gke =
-            (prev.google-cloud-sdk.withExtraComponents [
-              prev.google-cloud-sdk.components.gke-gcloud-auth-plugin
-              prev.google-cloud-sdk.components.config-connector
-            ])
-            .overrideAttrs (oa: {
-              buildCommand =
-                oa.buildCommand
-                + ''
-                  if [ -e $out/google-cloud-sdk/bin/config-connector ]; then
-                    echo patching config-connector binary
-                    chmod +w $out/google-cloud-sdk/bin/config-connector
-                    ${prev.patchelf}/bin/patchelf --set-interpreter "${prev.gcc}/nix-support/dynamic-linker" $out/google-cloud-sdk/bin/config-connector
-                    chmod 744 $out/google-cloud-sdk/bin/config-connector
-                  fi
-                '';
-            });
+          google-cloud-sdk-gke = prev.google-cloud-sdk.withExtraComponents [
+            prev.google-cloud-sdk.components.gke-gcloud-auth-plugin
+            prev.google-cloud-sdk.components.config-connector
+          ];
         })
       ];
     };
