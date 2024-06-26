f28d744e09b1e99219474d43b11691907b2accca
John Axel Eriksson <john@insane.se>
2024-06-26 15:03:10 +0200

    bugfix: gex44 already had intelgpu

--- profiles/hardware/gex44.nix
+++ profiles/hardware/gex44.nix
@@ -2,7 +2,6 @@
   imports = [
     ./intel.nix
     inputs.nixos-hardware.nixosModules.common-cpu-intel
-    inputs.nixos-hardware.nixosModules.common-gpu-intel
     inputs.nixos-hardware.nixosModules.common-gpu-nvidia
   ];
 }
