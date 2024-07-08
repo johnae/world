5dbe16b6d8dafaa05f4944eb3dc6383c0281e1d0
John Axel Eriksson <john@insane.se>
2024-07-09 00:03:42 +0200

    chore: cygnus tweak

--- hosts/x86_64-linux/cygnus.nix
+++ hosts/x86_64-linux/cygnus.nix
@@ -30,7 +30,7 @@
 
   boot.initrd = {
     systemd.enable = true;
-    luks.devices.cryptkey.crypttabExtraOpts = ["fido2-device=auto"];
+    #luks.devices.cryptkey.crypttabExtraOpts = ["fido2-device=auto"];
   };
 
   age.secrets = {
