93da8601a292cf872576abe9aa2a2c7cb6aba6df
John Axel Eriksson <john@insane.se>
2024-08-19 13:36:32 +0200

    chore: temp fixes waiting for upstream

--- flake/packages.nix
+++ flake/packages.nix
@@ -48,6 +48,53 @@
           p:
             map tofuProvider [p.null p.external p.hcloud p.cloudflare p.random p.tailscale]
         );
+        delta = pkgs.rustPlatform.buildRustPackage rec {
+          pname = "delta";
+          version = "0.17.0-unstable-2024-08-12";
+
+          src = pkgs.fetchFromGitHub {
+            owner = "dandavison";
+            repo = pname;
+            rev = "a01141b72001f4c630d77cf5274267d7638851e4";
+            hash = "sha256-My51pQw5a2Y2VTu39MmnjGfmCavg8pFqOmOntUildS0=";
+          };
+
+          cargoHash = "sha256-Rlc3Bc6Jh89KLLEWBWQB5GjoeIuHnwIVZN/MVFMjY24=";
+
+          nativeBuildInputs = [
+            pkgs.installShellFiles
+            pkgs.pkg-config
+          ];
+
+          buildInputs = [
+            pkgs.oniguruma
+          ];
+
+          nativeCheckInputs = [pkgs.git];
+
+          env = {
+            RUSTONIG_SYSTEM_LIBONIG = true;
+          };
+
+          postInstall = ''
+            installShellCompletion --cmd delta \
+              etc/completion/completion.{bash,fish,zsh}
+          '';
+
+          # test_env_parsing_with_pager_set_to_bat sets environment variables,
+          # which can be flaky with multiple threads:
+          # https://github.com/dandavison/delta/issues/1660
+          dontUseCargoParallelTests = true;
+
+          meta = with lib; {
+            homepage = "https://github.com/dandavison/delta";
+            description = "Syntax-highlighting pager for git";
+            changelog = "https://github.com/dandavison/delta/releases/tag/${version}";
+            license = licenses.mit;
+            # maintainers = with maintainers; [zowoq SuperSandro2000 figsoda];
+            mainProgram = "delta";
+          };
+        };
         rbw = pkgs.runCommand "rbw" {} ''
           mkdir -p $out/bin
           tar -zxf ${inputs.rbw-static-linux-amd64} -C $out/bin
