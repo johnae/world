ce42a77e40d1cc6be7900c2afed47eb2ba46ade9
John Axel Eriksson <john@insane.se>
2024-08-20 11:09:52 +0200

    fix: remove temporary workarounds

--- flake/setup.nix
+++ flake/setup.nix
@@ -19,17 +19,6 @@
         inputs.nur.overlay
         (_final: _prev: (filterAttrs (name: _: ((match "nu-.*" name == null) && (match "nu_.*" name == null))) config.packages))
         (_final: prev: {
-          awscli2 = prev.awscli2.overrideAttrs (oa: {
-            patches = [
-              # Temporary test fix until https://github.com/aws/aws-cli/pull/8838 is merged upstream
-              (prev.fetchpatch {
-                url = "https://github.com/aws/aws-cli/commit/b5f19fe136ab0752cd5fcab21ff0ab59bddbea99.patch";
-                hash = "sha256-NM+nVlpxGAHVimrlV0m30d4rkFVb11tiH8Y6//2QhMI=";
-              })
-            ];
-          });
-        })
-        (_final: prev: {
           wlroots_river_0_17_2 = prev.wlroots_0_17.overrideAttrs (oa: rec {
             version = "0.17.2";
             src = prev.fetchFromGitLab {
