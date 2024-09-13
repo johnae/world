7b67ad538b3d65cdee3438c2dadfeb4a115b6f40
John Axel Eriksson <john@insane.se>
2024-09-13 23:23:37 +0200

    chore: tweaks

--- users/profiles/wezterm/default.nix
+++ users/profiles/wezterm/default.nix
@@ -10,10 +10,10 @@
       local function getinfo()
       end
       _G.debug = {getinfo = getinfo}
+      _G.wezterm = wezterm
+      _G.dev_remote = "${config.userinfo.devRemote}"
       local fennel = dofile("${pkgs.lua54Packages.fennel}/share/lua/5.4/fennel.lua")
       wezterm.log_error("loaded fennel")
-      _G["wezterm"] = wezterm
-      _G["dev_remote"] = "${config.userinfo.devRemote}"
       local config = fennel.dofile("${./wezterm.fnl}")
       wezterm.log_error("evaluated wezterm.fnl")
       return config
