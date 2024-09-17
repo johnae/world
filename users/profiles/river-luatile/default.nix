50d6461f1b72033b8b5a1713744fc90120b5f0ae
John Axel Eriksson <john@insane.se>
2024-09-17 15:21:47 +0200

    chore: wezterm/luatile load compiled fennel by default

--- users/profiles/river-luatile/default.nix
+++ users/profiles/river-luatile/default.nix
@@ -3,15 +3,25 @@
   pkgs,
   ...
 }: let
-  inherit (config.xdg) configHome;
+  inherit (config) xdg;
 in {
-  xdg.configFile."river-luatile/layout.fnl".source = ./layout.fnl;
+  xdg.configFile."river-luatile/layout.fnl.lua".source =
+    pkgs.runCommand "layout.fnl.lua" {
+      nativeBuildInputs = [pkgs.fennel];
+    }
+    ''
+      fennel --compile ${./layout.fnl} > $out
+    '';
   xdg.configFile."river-luatile/layout.lua".source = pkgs.writeText "layout.lua" ''
-    -- fake
-    local function getinfo()
+    if os.getenv("LUATILE_FNL") then
+      -- fake
+      local function getinfo()
+      end
+      _G.debug = {getinfo = getinfo}
+      local fennel = dofile("${pkgs.lua54Packages.fennel}/share/lua/5.4/fennel.lua")
+      fennel.dofile(os.getenv("LUATILE_FNL"))
+    else
+      dofile("${xdg.configFile."river-luatile/layout.fnl.lua".target}")
     end
-    _G.debug = {getinfo = getinfo}
-    local fennel = dofile("${pkgs.lua54Packages.fennel}/share/lua/5.4/fennel.lua")
-    fennel.dofile("${configHome}/river-luatile/layout.fnl")
   '';
 }
--- users/profiles/wezterm/default.nix
+++ users/profiles/wezterm/default.nix
@@ -2,20 +2,32 @@
   config,
   pkgs,
   ...
-}: {
+}: let
+  inherit (config) xdg;
+in {
+  xdg.configFile."wezterm/wezterm.fnl.lua".source =
+    pkgs.runCommand "wezterm.fnl.lua" {
+      nativeBuildInputs = [pkgs.fennel];
+    }
+    ''
+      fennel --compile ${./wezterm.fnl} > $out
+    '';
   programs.wezterm = {
     enable = true;
     extraConfig = ''
-      -- fake
-      local function getinfo()
-      end
-      _G.debug = {getinfo = getinfo}
       _G.wezterm = wezterm
       _G.dev_remote = "${config.userinfo.devRemote}"
-      local fennel = dofile("${pkgs.lua54Packages.fennel}/share/lua/5.4/fennel.lua")
-      wezterm.log_error("loaded fennel")
-      local config = fennel.dofile("${./wezterm.fnl}")
-      wezterm.log_error("evaluated wezterm.fnl")
+      local config
+      if os.getenv("WEZTERM_FNL") then
+        -- fake
+        local function getinfo()
+        end
+        _G.debug = {getinfo = getinfo}
+        local fennel = dofile("${pkgs.lua54Packages.fennel}/share/lua/5.4/fennel.lua")
+        config = fennel.dofile(os.getenv("WEZTERM_FNL"))
+      else
+        config = dofile("${xdg.configFile."wezterm/wezterm.fnl.lua".target}")
+      end
       return config
     '';
   };
