749a227c7865c89a6d03334838da53e051e89c62
John Axel Eriksson <john@insane.se>
2024-12-12 23:08:23 +0100

    fix: cloudflare-ts infinite recursion (#1097)

--- modules/cloudflare-ts.nix
+++ modules/cloudflare-ts.nix
@@ -9,7 +9,7 @@
   cfg = config.services.cloudflare-tailscale-dns;
 in {
   options.services.cloudflare-tailscale-dns = lib.mkOption {
-    type = lib.attrsOf (lib.submodule ({name, ...}: {
+    type = types.attrsOf (types.submodule ({name, ...}: {
       options = {
         enable = lib.mkEnableOption "Enable this cloudflare dns tailscale mapping";
         delete = lib.mkOption {
@@ -31,36 +31,36 @@ in {
     default = {};
   };
 
-  config = lib.mkMerge (lib.mapAttrsToList (
-      name: value: {
-        systemd.services."cloudflare-tailscale-dns-${name}.${value.zone}" = {
-          description = "Cloudflare dns records mapping ${name}.${value.zone} to host tailscale ip";
-          inherit enable;
-          serviceConfig = {
-            Type = "oneshot";
-            RemainAfterExit = "yes";
-            EnvironmentFile = [
-              value.cloudflareEnvFile
-            ];
-          };
-          script = ''
-            TS_IP="$(${pkgs.tailscale}/bin/tailscale status --json | ${pkgs.jq}/bin/jq -r '.Self.TailscaleIPs[0]')"
-            ${
-              if value.delete
-              then ''
-                RECORD_ID="$(${pkgs.flarectl}/bin/flarectl --json dns list --zone 9000.dev | ${pkgs.jq}/bin/jq -r '.[] | select(.Name == "${cfg.name}.${cfg.zone}") | .ID')"
-                ${pkgs.flarectl}/bin/flarectl dns delete --zone ${value.zone} --id "$RECORD_ID"
-              ''
-              else ''
-                ${pkgs.flarectl}/bin/flarectl dns create-or-update --zone ${value.zone} --name "${cfg.name}" --type A --ttl 60 --content "$TS_IP"
-              ''
-            }
-          '';
-          after = ["network-online.target" "tailscale-auth.service"];
-          requires = ["network-online.target" "tailscale-auth.service"];
-          wantedBy = ["multi-user.target"];
+  config.systemd.services =
+    lib.mapAttrs' (name: value: {
+      name = "cloudflare-tailscale-dns-${name}.${value.zone}";
+      value = {
+        description = "Cloudflare dns records mapping ${name}.${value.zone} to host tailscale ip";
+        inherit enable;
+        serviceConfig = {
+          Type = "oneshot";
+          RemainAfterExit = "yes";
+          EnvironmentFile = [
+            value.cloudflareEnvFile
+          ];
         };
-      }
-    )
-    cfg.cloudflare-tailscale-dns);
+        script = ''
+          TS_IP="$(${pkgs.tailscale}/bin/tailscale status --json | ${pkgs.jq}/bin/jq -r '.Self.TailscaleIPs[0]')"
+          ${
+            if value.delete
+            then ''
+              RECORD_ID="$(${pkgs.flarectl}/bin/flarectl --json dns list --zone 9000.dev | ${pkgs.jq}/bin/jq -r '.[] | select(.Name == "${value.name}.${value.zone}") | .ID')"
+              ${pkgs.flarectl}/bin/flarectl dns delete --zone ${value.zone} --id "$RECORD_ID"
+            ''
+            else ''
+              ${pkgs.flarectl}/bin/flarectl dns create-or-update --zone ${value.zone} --name "${value.name}" --type A --ttl 60 --content "$TS_IP"
+            ''
+          }
+        '';
+        after = ["network-online.target" "tailscale-auth.service"];
+        requires = ["network-online.target" "tailscale-auth.service"];
+        wantedBy = ["multi-user.target"];
+      };
+    })
+    cfg;
 }
