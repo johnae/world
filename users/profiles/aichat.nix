896197a83519ffacf24d26b279ccebb8af5adcd8
John Axel Eriksson <john@insane.se>
2024-12-23 01:10:37 +0100

    feat: configure aichat properly (#1115)

--- users/profiles/aichat.nix
+++ users/profiles/aichat.nix
@@ -7,23 +7,38 @@
     if pkgs.stdenv.isDarwin && !config.xdg.enable
     then "Library/Application Support/aichat"
     else "${config.xdg.configHome}/aichat";
+  aichat = pkgs.writeShellApplication {
+    name = "aichat";
+    runtimeInputs = [pkgs.aichat];
+    text = ''
+      OPENAI_API_KEY="$(cat /run/agenix/openai-api-key)";
+      CLAUDE_API_KEY="$(cat /run/agenix/anthropic-api-key)";
+      ANTHROPIC_API_KEY="$(cat /run/agenix/anthropic-api-key)";
+      export OPENAI_API_KEY CLAUDE_API_KEY ANTHROPIC_API_KEY
+      exec aichat "$@"
+    '';
+  };
 in {
-  home.packages = [pkgs.aichat];
+  home.packages = [aichat];
   home.file."${configDir}/config.yaml".source = (pkgs.formats.yaml {}).generate "aichat-config.yaml" {
-    model = "ollama";
+    model = "openai:gpt-4o";
     stream = true;
+    keybindings = "vi";
+    function_calling = true;
     clients = [
       {
         type = "openai-compatible";
         name = "ollama";
         api_base = "http://ollama.9000.dev:11434/v1";
         api_key = null;
-        models = [
-          {
-            name = "qwen2.5-coder:32b";
-            max_input_tokens = 64000;
-          }
-        ];
+      }
+      {
+        type = "openai";
+        api_base = "https://api.openai.com/v1";
+      }
+      {
+        type = "claude";
+        api_base = "https://api.anthropic.com/v1";
       }
     ];
   };
