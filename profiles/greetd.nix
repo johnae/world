7c8fa8ef5a58e3fd81865e371da10e30933e8745
John Axel Eriksson <john@insane.se>
2025-01-05 14:48:54 +0100

    chore: default wayland session to sway

--- profiles/greetd.nix
+++ profiles/greetd.nix
@@ -218,7 +218,7 @@ in {
     enable = true;
     restart = true;
     settings = {
-      default_session.command = "${createGreeter "${runHyprland}/bin/hyprland" sessions}/bin/greeter";
+      default_session.command = "${createGreeter "${runSway}/bin/sway" sessions}/bin/greeter";
     };
   };
   ## prevents systemd spewing the console with log messages when greeter is active
