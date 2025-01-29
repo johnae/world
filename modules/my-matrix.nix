dc916048d44b82bc890c7afa0573754f5c560a52
John Axel Eriksson <john@insane.se>
2025-01-29 01:16:01 +0100

    fix(conduit): use freeform conf (#1237)

--- modules/my-matrix.nix
+++ modules/my-matrix.nix
@@ -33,73 +33,69 @@ in
       };
     };
 
-    config =
-      mkIf config.services.my-matrix.enable {
-        environment.persistence."/keep".directories = [
-          "/var/lib/private/matrix-conduit"
-        ];
-        services.matrix-conduit.enable = true;
+    config = mkIf config.services.my-matrix.enable {
+      environment.persistence."/keep".directories = [
+        "/var/lib/private/matrix-conduit"
+      ];
+      services.matrix-conduit.enable = true;
 
-        systemd.services.conduit.serviceConfig.ExecStart = lib.mkForce "${cfg.package}/bin/conduwuit";
+      systemd.services.conduit.serviceConfig.ExecStart = lib.mkForce "${cfg.package}/bin/conduwuit";
 
-        services.matrix-conduit.settings = {
-          global = {
-            inherit (config.services.my-matrix) server_name;
-            allow_federation = true;
-            allow_encryption = true;
-            allow_registration = true;
-            max_request_size = 20000000;
-            port = 6167;
-            database_backend = "rocksdb";
-          };
-          extraEnvironment = {
-            CONDUWUIT_MAX_CONCURRENT_REQUESTS = "100";
-            CONDUWUIT_TURN_URIS = "[\"turn:staticauth.turn.openrelay.metered.ca:443?transport=udp\+
-\", \"turn:staticauth.turn.openrelay.metered.ca:443?transport=tcp\"]";
-            CONDUWUIT_TURN_SECRET = "openrelayprojectsecret";
-            CONDUWUIT_REGISTRATION_TOKEN_FILE = config.age.secrets.conduwuit-registration-token.path;
-          };
+      services.matrix-conduit.settings = {
+        global = {
+          inherit (config.services.my-matrix) server_name;
+          allow_federation = true;
+          allow_encryption = true;
+          allow_registration = true;
+          registration_token_file = config.age.secrets.conduwuit-registration-token.path;
+          max_request_size = 20000000;
+          max_concurrent_requests = 100;
+          turn_uris = ["turn:staticauth.turn.openrelay.metered.ca:443?transport=udp" "turn:staticauth.turn.openrelay.metered.ca:443?transport=tcp"];
+          turn_secret = "openrelayprojectsecret";
+          port = 6167;
+          database_backend = "rocksdb";
         };
-        services.matrix-conduit.package = pkgs.conduwuit;
-        services.nginx.virtualHosts = {
-          "matrix.${cfg.settings.global.server_name}" = {
-            locations."/_matrix" = {
-              proxyPass = "http://backend_conduit$request_uri";
-              proxyWebsockets = true;
-              extraConfig = ''
-                proxy_set_header Host $host;
-                proxy_buffering off;
-              '';
-            };
+      };
+      services.matrix-conduit.package = pkgs.conduwuit;
+      services.nginx.virtualHosts = {
+        "matrix.${cfg.settings.global.server_name}" = {
+          locations."/_matrix" = {
+            proxyPass = "http://backend_conduit$request_uri";
+            proxyWebsockets = true;
             extraConfig = ''
-              merge_slashes off;
+              proxy_set_header Host $host;
+              proxy_buffering off;
             '';
           };
-          "${cfg.settings.global.server_name}" = {
-            locations."=/.well-known/matrix/server" = {
-              alias = "${well_known_server}";
-
-              extraConfig = ''
-                default_type application/json;
-              '';
-            };
-
-            locations."=/.well-known/matrix/client" = {
-              alias = "${well_known_client}";
-
-              extraConfig = ''
-                default_type application/json;
-                add_header Access-Control-Allow-Origin "*";
-              '';
-            };
-          };
+          extraConfig = ''
+            merge_slashes off;
+          '';
         };
-        services.nginx.upstreams = {
-          "backend_conduit" = {
-            servers = {
-              "[::1]:${toString cfg.settings.global.port}" = {};
-            };
+        "${cfg.settings.global.server_name}" = {
+          locations."=/.well-known/matrix/server" = {
+            alias = "${well_known_server}";
+
+            extraConfig = ''
+              default_type application/json;
+            '';
+          };
+
+          locations."=/.well-known/matrix/client" = {
+            alias = "${well_known_client}";
+
+            extraConfig = ''
+              default_type application/json;
+              add_header Access-Control-Allow-Origin "*";
+            '';
           };
         };
       };
+      services.nginx.upstreams = {
+        "backend_conduit" = {
+          servers = {
+            "[::1]:${toString cfg.settings.global.port}" = {};
+          };
+        };
+      };
+    };
   }
