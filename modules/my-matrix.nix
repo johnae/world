7bbfa05cfcb901aa0181be30166dfd5eee9fa8a4
John Axel Eriksson <john@insane.se>
2025-01-29 01:01:27 +0100

    fix(conduit): actually allow registration

--- modules/my-matrix.nix
+++ modules/my-matrix.nix
@@ -47,7 +47,7 @@ in
             inherit (config.services.my-matrix) server_name;
             allow_federation = true;
             allow_encryption = true;
-            allow_registration = false;
+            allow_registration = true;
             max_request_size = 20000000;
             port = 6167;
             database_backend = "rocksdb";
@@ -57,7 +57,6 @@ in
             CONDUWUIT_TURN_URIS = "[\"turn:staticauth.turn.openrelay.metered.ca:443?transport=udp\+
 \", \"turn:staticauth.turn.openrelay.metered.ca:443?transport=tcp\"]";
             CONDUWUIT_TURN_SECRET = "openrelayprojectsecret";
-            CONDUWUIT_ALLOW_REGISTRATION = "true";
             CONDUWUIT_REGISTRATION_TOKEN_FILE = config.age.secrets.conduwuit-registration-token.path;
           };
         };
