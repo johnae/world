c04259e8e46dad2327e24554b48a4dce01ce0eb3
John Axel Eriksson <john@insane.se>
2025-02-10 23:36:20 +0100

    feat(forgejo): enable forgejo on icarus

---
+++ profiles/forgejo.nix
@@ -0,0 +1,26 @@
+{config, ...}: let
+  cfg = config.services.forgejo;
+  srv = cfg.settings.server;
+in {
+  services.forgejo.enable = true;
+  services.forgejo.settings = {
+    server = {
+      DOMAIN = "git.9000.dev";
+      ROOT_URL = "https://${srv.DOMAIN}";
+    };
+    service.DISABLE_REGISTRATION = true;
+  };
+  environment.persistence."/keep".directories = [
+    cfg.stateDir
+  ];
+  services.restic.backups.remote.paths = [
+    cfg.stateDir
+  ];
+  services.nginx.virtualHosts = {
+    "${srv.DOMAIN}" = {
+      locations."/" = {
+        proxyPass = "http://localhost:3000";
+      };
+    };
+  };
+}
