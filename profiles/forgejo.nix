1c2a36aa04be3846ad70109bb20daca1aec21205
John Axel Eriksson <john@insane.se>
2025-02-15 14:24:51 +0100

    fix(forgejo): use alt port

--- profiles/forgejo.nix
+++ profiles/forgejo.nix
@@ -7,6 +7,7 @@ in {
     server = {
       DOMAIN = "git.9000.dev";
       ROOT_URL = "https://${srv.DOMAIN}";
+      HTTP_PORT = 3121;
     };
     service.DISABLE_REGISTRATION = true;
   };
@@ -19,7 +20,7 @@ in {
   services.nginx.virtualHosts = {
     "${srv.DOMAIN}" = {
       locations."/" = {
-        proxyPass = "http://localhost:3000";
+        proxyPass = "http://localhost:3121";
       };
     };
   };
