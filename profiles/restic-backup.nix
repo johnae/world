e16ffb1c25345f78d854d75d09ffb54263088f1a
John Axel Eriksson <john@insane.se>
2025-03-13 11:03:48 +0100

    feat(restic): second backup destination

--- profiles/restic-backup.nix
+++ profiles/restic-backup.nix
@@ -12,10 +12,38 @@
       file = ../secrets/restic-pw.age;
       owner = "1337";
     };
+    restic-local-env = {
+      file = ../secrets/restic-local.age;
+      owner = "1337";
+    };
+    restic-local-pw = {
+      file = ../secrets/restic-local-pw.age;
+      owner = "1337";
+    };
   };
 
   services.restic = {
     backups = {
+      local = {
+        paths = [
+          "/home/${adminUser.name}/Development"
+          "/home/${adminUser.name}/Documents"
+          "/home/${adminUser.name}/Sync"
+          "/home/${adminUser.name}/Photos"
+          "/home/${adminUser.name}/Mail"
+        ];
+        environmentFile = config.age.secrets.restic-local-env.path;
+        passwordFile = config.age.secrets.restic-local-pw.path;
+        repository = "s3:https://storage.9000.dev/backup";
+        initialize = true;
+        timerConfig.OnCalendar = "*-*-* *:00:00";
+        timerConfig.RandomizedDelaySec = "5m";
+        extraBackupArgs = [
+          "--exclude=\".direnv\""
+          "--exclude=\".terraform\""
+          "--exclude=\"node_modules/*\""
+        ];
+      };
       remote = {
         paths = [
           "/home/${adminUser.name}/Development"
