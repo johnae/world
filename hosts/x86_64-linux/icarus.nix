1753c69d85c410ed5f39a0aed0039e0f7bcfba0a
John Axel Eriksson <john@insane.se>
2025-03-18 23:00:35 +0100

    fix(tailscale/icarus): accept dns

--- hosts/x86_64-linux/icarus.nix
+++ hosts/x86_64-linux/icarus.nix
@@ -137,7 +137,7 @@
     args.advertise-tags = ["tag:server"];
     args.ssh = true;
     args.accept-routes = false;
-    args.accept-dns = false;
+    args.accept-dns = true;
     args.advertise-exit-node = true;
     args.auth-key = config.age.secrets.ts-google-9k.path;
   };
