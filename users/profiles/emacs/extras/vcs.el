96b25df23e8311fdd2afd1b314bc8559210dc1cc
John Axel Eriksson <john@insane.se>
2025-08-27 15:26:48 +0200

    feat(emacs): fix forced project selection and commit mode

--- users/profiles/emacs/extras/vcs.el
+++ users/profiles/emacs/extras/vcs.el
@@ -1,28 +1,46 @@
 (use-package git-commit
   :ensure t
-  :defer t)
+  :config
+  ;; Recognize JJ temp files as commit messages too
+  (setq git-commit-filename-regexp
+        (concat git-commit-filename-regexp
+                "\\|/editor-.*\\.jjdescription\\'"))
+  
+  ;; Explicitly add file associations for commit messages
+  (add-to-list 'auto-mode-alist '("/COMMIT_EDITMSG\\'" . git-commit-mode))
+  (add-to-list 'auto-mode-alist '("/MERGE_MSG\\'" . git-commit-mode))
+  (add-to-list 'auto-mode-alist '("/editor-.*\\.jjdescription\\'" . git-commit-mode)))
 
-(add-to-list 'auto-mode-alist
-             '("COMMIT_EDITMSG\\'" . git-commit-mode))
+;; Debug code - commented out after fixing project prompt issues
+;; (defun my/debug-project-switch (orig-fun &rest args)
+;;   (message ">>> project-switch-project called with %S" args)
+;;   (backtrace)
+;;   (apply orig-fun args))
+;; (advice-add 'project-switch-project :around #'my/debug-project-switch)
 
-(add-to-list 'auto-mode-alist
-             '("/editor-.*\\.jjdescription\\'" . git-commit-mode))
+(defun my/project-ignore-commit-buffers (dir)
+  "Force JJ/Git commit buffers into a safe dummy project."
+  (let ((fname (or (buffer-file-name (buffer-base-buffer)) "")))
+    (when (or (string-match-p "COMMIT_EDITMSG\\'" fname)
+              (string-match-p "/editor-.*\\.jjdescription\\'" fname))
+      ;; A fake 'vc project rooted in /tmp that never prompts
+      (cons 'vc "/tmp"))))
 
-(defun my/project-try-commit (dir)
-  "If DIR is a Git or Jujutsu commit message, mark it as no-project."
-  (let ((fname (buffer-file-name (buffer-base-buffer))))
-    (when (and fname
-               (string-match-p "\\(COMMIT_EDITMSG\\|\\.jjdescription\\)\\'" fname))
-      ;; Return a dummy project cons cell with custom type 'no-project
-      (cons 'no-project (file-name-directory fname)))))
+;; Add with negative priority to run earlier than other project finders
+(add-hook 'project-find-functions #'my/project-ignore-commit-buffers -90)
 
-(add-hook 'project-find-functions #'my/project-try-commit)  ;; Prepend our detector
+(defun my/setup-commit-buffer-keys ()
+  "Setup keybindings for both git and jj commit buffers."
+  ;; Ensure with-editor-mode is active
+  (when (fboundp 'with-editor-mode)
+    (with-editor-mode 1))
+  ;; Setup standard commit keybindings
+  (local-set-key (kbd "C-c C-c") #'server-edit)
+  (local-set-key (kbd "C-c C-k")
+                 (lambda ()
+                   (interactive)
+                   (set-buffer-modified-p nil)
+                   (server-edit-abort))))
 
-(cl-defmethod project-root ((project (head no-project)))
-  "Return the root directory of a no-project PROJECT."
-  (cdr project))
-
-(cl-defmethod project-files ((project (head no-project)) &optional _dirs)
-  "No files in a no-project project."
-  ;; Return an empty list to avoid triggering any ‘find’ commands
-  '())
+;; Hook this to git-commit-mode, which now activates for both git and jj
+(add-hook 'git-commit-setup-hook #'my/setup-commit-buffer-keys)
