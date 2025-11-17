7f2f9e4c79d72f163400b3fa26e4812c6b2727a7
John Axel Eriksson <john@insane.se>
2025-11-18 00:01:42 +0100

    feat(zen): add zen browser

---
+++ users/profiles/zen.nix
@@ -0,0 +1,24 @@
+{pkgs, ...}: {
+  programs.zen-browser = {
+    enable = true;
+    nativeMessagingHosts = [pkgs.firefoxpwa];
+    policies = {
+      AutofillAddressEnabled = true;
+      AutofillCreditCardEnabled = false;
+      DisableAppUpdate = true;
+      DisableFeedbackCommands = true;
+      DisableFirefoxStudies = true;
+      DisablePocket = true;
+      DisableTelemetry = true;
+      DontCheckDefaultBrowser = true;
+      NoDefaultBookmarks = true;
+      OfferToSaveLogins = false;
+      EnableTrackingProtection = {
+        Value = true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+      };
+    };
+  };
+}
