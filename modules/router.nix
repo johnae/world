991ce7a9f1c912fcdd2731de2143ec8acbb5a035
John Axel Eriksson <john@insane.se>
2025-11-21 00:19:34 +0100

    fix(router): cname cloaking blocklist (#1616)

--- modules/router.nix
+++ modules/router.nix
@@ -170,6 +170,7 @@ in {
         blocking = {
           denylists = {
             ads = [
+              "https://raw.githubusercontent.com/nextdns/cname-cloaking-blocklist/master/domains"
               "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/pro.plus.txt"
               "https://raw.githubusercontent.com/lassekongo83/Frellwits-filter-lists/master/Frellwits-Swedish-Hosts-File.txt"
               "https://pgl.yoyo.org/adservers/serverlist.php?showintro=0;hostformat=hosts"
