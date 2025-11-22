92bcab91181f2e2db42da28a8b9f8e01018900be
John Axel Eriksson <john@insane.se>
2025-11-22 22:37:41 +0100

    fix(cleanup): fix auto-updates, remove unnecessary pkgs

--- flake/packages.nix
+++ flake/packages.nix
@@ -51,7 +51,6 @@
         helix-latest = inputs.helix-editor.packages.${system}.default;
         zjstatus = inputs.zjstatus.packages.${system}.default;
         zj-which-key = inputs.zj-which-key.packages.${system}.default;
-        quickshell = inputs.quickshell.packages.${system}.default;
         noctalia = inputs.noctalia.packages.${system}.default;
         ## fix this one on darwin
         zwift =
